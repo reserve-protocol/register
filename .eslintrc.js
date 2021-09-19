180c9a73d23327c79cf0ca7ce7fe450b338164f3
Luis Camargo <luiska1993@gmail.com>
2021-09-19 12:53:09 -0500

    chore: remove deprecated prettier-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'plugin:cypress/recommended',
     'airbnb',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
