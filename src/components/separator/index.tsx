bbd1a874cfad6d5122bb55c8b1499acf2e16e940
Luis Camargo <luiska1993@gmail.com>
2022-01-18 18:40:33 -0600

    fix: downgrade react-overlay

--- src/components/separator/index.tsx
+++ src/components/separator/index.tsx
@@ -2,8 +2,6 @@ import { Box } from 'theme-ui'
 import styled from '@emotion/styled'
 
 const Separator = styled(Box)`
-  height: 100%;
-  margin: 16px auto;
   background-color: #e4e5e7;
   width: 1px;
 `
--- src/views/overview/components/assets/index.tsx
+++ src/views/overview/components/assets/index.tsx
@@ -1,12 +1,61 @@
-import { Box, Grid, Text } from '@theme-ui/components'
+import { Box, Flex, Grid, Text } from '@theme-ui/components'
 import { Card } from 'components'
 import InfoBox from 'components/info-box'
 import Separator from 'components/separator'
 import { formatEther } from 'ethers/lib/utils'
 import useTokenSupply from 'hooks/useTokenSupply'
 import { useEffect, useState } from 'react'
+import { Pie, PieChart } from 'recharts'
 import { ReserveToken } from 'types'
 import { formatCurrency } from 'utils'
+
+const data02 = [
+  {
+    name: 'Group A',
+    value: 2400,
+  },
+  {
+    name: 'Group B',
+    value: 4567,
+  },
+  {
+    name: 'Group C',
+    value: 1398,
+  },
+  {
+    name: 'Group D',
+    value: 9800,
+  },
+  {
+    name: 'Group E',
+    value: 3908,
+  },
+  {
+    name: 'Group F',
+    value: 4800,
+  },
+]
+const mock = [{ name: 'test', value: 'holi' }]
+
+const AssetsChart = () => {
+  console.log('works?')
+  return (
+    <Box p={2}>
+      <PieChart width={100} height={100}>
+        <Pie
+          data={data02}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          innerRadius={38}
+          outerRadius={50}
+          fill="#82ca9d"
+        />
+      </PieChart>
+    </Box>
+  )
+}
 
 /**
  * RToken Assets overview
@@ -52,28 +101,38 @@ const AssetsOverview = ({
   }, [marketCap])
 
   return (
-    <Box {...props}>
+    <Box mb={3} {...props}>
       <Text sx={{ fontSize: 3, display: 'block' }} mb={2}>
         Assets
       </Text>
-      <Card p={4}>
-        <InfoBox
-          title={`${
-            marketCap
-              ? formatCurrency(parseFloat(formatEther(marketCap)))
-              : '0.00'
-          } ${token.symbol}`}
-          subtitle="In circulation"
-        />
-        <Separator />
+      <Card px={4} py={3}>
+        <Flex>
+          <Box sx={{ borderRight: '1px solid #e4e5e7' }} pr={4} mr={4}>
+            <Text sx={{ color: '#77838F', display: 'block', fontSize: 1 }}>
+              Total circulation
+            </Text>
+            <Text sx={{ fontSize: 3 }}>
+              {marketCap
+                ? formatCurrency(parseFloat(formatEther(marketCap)))
+                : '0.00'}{' '}
+              {token.symbol}
+            </Text>
+          </Box>
+          <Box>
+            <Text sx={{ color: '#77838F', display: 'block', fontSize: 1 }}>
+              Total collateral assets
+            </Text>
+            <AssetsChart />
+          </Box>
+        </Flex>
       </Card>
-      <Grid columns={3} gap={0} mb={5}>
+      {/* <Grid columns={3} gap={0} mb={5}>
         {collaterals.map((collateral) => (
           <Card key={collateral.address}>
             <InfoBox title={collateral.value} subtitle={collateral.name} />
           </Card>
         ))}
-      </Grid>
+      </Grid> */}
     </Box>
   )
 }
