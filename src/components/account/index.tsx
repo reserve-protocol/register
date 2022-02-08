1023599b412f273b65691214efadb0e85a3b7c98
Luis Camargo <luiska1993@gmail.com>
2022-02-08 09:55:47 -0600

    chore: replace jazzicon for blockies

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
-import Jazzicon from '@metamask/jazzicon'
 import { shortenAddress, useEthers } from '@usedapp/core'
 import useENSName from 'hooks/ens/useENSName'
-import { useEffect, useRef, useState } from 'react'
-import { Button, Text } from 'theme-ui'
+import Blockies from 'react-blockies'
+import { useState } from 'react'
+import { Button, Text, Box } from '@theme-ui/components'
 import WalletModal from '../wallet-modal'
 
 const Container = styled.div`
@@ -12,31 +12,10 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   height: 38px;
-  padding: 1rem;
+  padding: 8px;
   border-radius: 4px;
   cursor: pointer;
 `
-
-const StyledIdenticon = styled.div`
-  margin-right: 10px;
-  margin-top: 4px;
-  border-radius: 1.125rem;
-`
-
-function Identicon() {
-  const ref = useRef<HTMLDivElement>()
-  const { account } = useEthers()
-
-  useEffect(() => {
-    if (account && ref.current) {
-      ref.current.innerHTML = ''
-      ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)))
-    }
-  }, [account])
-
-  return <StyledIdenticon ref={ref as any} />
-}
-
 /**
  * Account
  *
@@ -62,7 +41,9 @@ const Account = () => {
         </Button>
       ) : (
         <Container onClick={handleOpenModal}>
-          <Identicon />
+          <Box mr={2} mt={1}>
+            <Blockies scale={3} seed={account?.slice(2, 10) ?? ''} />
+          </Box>
           <Text>{ENSName || shortenAddress(account)}</Text>
         </Container>
       )}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,1 @@
 /// <reference types="react-scripts" />
-declare module '@metamask/jazzicon' {
-  // eslint-disable-next-line no-unused-vars
-  export default function (diameter: number, seed: number): HTMLElement
-}
