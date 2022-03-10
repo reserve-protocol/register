2c654155879ed795a608686d9911e3cb3e031a43
Luis Camargo <luiska1993@gmail.com>
2022-03-09 20:31:23 -0600

    chore: initial layout

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,24 +1,52 @@
 import { Grid, Text, Box } from '@theme-ui/components'
 import { Container } from 'components'
+import { Table } from 'components/table'
 
-const mockData = [
+const mockTokens: any = [
+  { name: 'USD1', price: '$1.00', balance: '0.00', value: '0.00', apy: '0.00' },
   {
-    token: {
-      symbol: 'Test',
-      name: 'Test',
-      address: '1234',
-      balance: 1234,
-    },
-    insurance: {
-      token: {
-        symbol: 'rsr+test',
-        name: 'rsr+test',
-        address: '1234',
-        balance: '1234',
-      },
-    },
-    rsrBalance: 1234,
+    name: 'USD1',
+    price: '$1.00',
+    balance: '0.00',
+    value: '$1,308.38',
+    apy: '3.05',
   },
+]
+
+const stakingTokens: any = [
+  {
+    name: 'USD1RSR',
+    rate: '1.024700',
+    balance: '10,234,400',
+    rsrValue: '10,487,190',
+    usdValue: '200,743.79',
+    apy: '5.45%',
+  },
+  {
+    name: 'USD1RSR',
+    rate: '1.013800',
+    balance: '0.00',
+    rsrValue: '0',
+    usdValue: '$0',
+    apy: '5.04%',
+  },
+]
+
+const yourTokensColumns: any = [
+  { Header: 'RToken', accessor: 'name' },
+  { Header: 'Price', accessor: 'price' },
+  { Header: 'Balance', accessor: 'balance' },
+  { Header: 'Value', accessor: 'value' },
+  { Header: 'APY', accessor: 'apy' },
+]
+
+const yourStakingTokensColumns: any = [
+  { Header: 'IP Token', accessor: 'name' },
+  { Header: 'RSR Rate', accessor: 'rate' },
+  { Header: 'Balance', accessor: 'balance' },
+  { Header: 'RSR Value', accessor: 'rsrValue' },
+  { Header: 'USD Value', accessor: 'usdValue' },
+  { Header: 'APY', accessor: 'apy' },
 ]
 
 const Home = () => {
@@ -27,10 +55,21 @@ const Home = () => {
   return (
     <Container>
       <Text sx={{ fontSize: 6, fontWeight: 100 }}>$ 123,123.00</Text>
-      <Grid columns={2}>
-        <Box></Box>
-        <Box></Box>
+      <Grid columns={2} mt={4}>
+        <Box>
+          <Text mb={3} variant="sectionTitle">
+            Your RTokens
+          </Text>
+          <Table columns={yourTokensColumns} data={mockTokens} />
+        </Box>
+        <Box>
+          <Text mb={3} variant="sectionTitle">
+            Your IP Tokens
+          </Text>
+          <Table columns={yourStakingTokensColumns} data={stakingTokens} />
+        </Box>
       </Grid>
+      <Box>{/* <Table /> */}</Box>
     </Container>
   )
 }
