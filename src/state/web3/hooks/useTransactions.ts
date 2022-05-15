102ce9a12199a9a5b6eba27ff5fac7422c19b37d
Luis Camargo <luiska1993@gmail.com>
2022-05-15 12:27:38 -0500

    feat: improve useTransaction hokos

--- src/state/web3/hooks/useTransactions.ts
+++ src/state/web3/hooks/useTransactions.ts
@@ -1,14 +1,23 @@
+import { useMemo } from 'react'
 import { currentTxAtom } from 'state/atoms'
 import { useAtomValue } from 'jotai/utils'
 
 export const useTransactions = (ids: string[]) => {
-  const txs = useAtomValue(currentTxAtom)
-
-  return txs.filter((tx) => ids.includes(tx.id))
+  // Usually used to fetch last N txs, slice it to last 20 for fast lookup
+  const txs = useAtomValue(currentTxAtom).slice(-20)
+  return useMemo(() => txs.filter((tx) => ids.includes(tx.id)), [txs, ids])
 }
 
 export const useTransaction = (id: string) => {
   const txs = useAtomValue(currentTxAtom)
 
-  return txs.find((tx) => tx.id === id)
+  return useMemo(() => {
+    // This hook is usually used to get the last txs,
+    // reverse lookup for performance makes sense
+    for (let i = txs.length - 1; i >= 0; --i) {
+      if (txs[i].id === id) return txs[i]
+    }
+
+    return []
+  }, [txs, id])
 }
