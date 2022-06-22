ddbbf4ecbfede66def7b7978a9da309779fcd3f8
Luis Camargo <luiska1993@gmail.com>
2022-06-22 18:38:33 -0500

    feat: eager connect

--- src/state/web3/components/WalletUpdater.tsx
+++ src/state/web3/components/WalletUpdater.tsx
@@ -1,11 +1,26 @@
 import { useWeb3React } from '@web3-react/core'
+import { Connector } from '@web3-react/types'
+import { metaMask, network } from 'components/wallets/connectors'
 import { useAtom } from 'jotai'
 import { useEffect } from 'react'
 import { walletAtom } from 'state/atoms'
 
+const connect = async (connector: Connector) => {
+  try {
+    if (connector.connectEagerly) {
+      await connector.connectEagerly()
+    } else {
+      await connector.activate()
+    }
+  } catch (error) {
+    console.debug(`web3-react eager connection error: ${error}`)
+  }
+}
+
 const WalletUpdater = () => {
-  const { account } = useWeb3React()
+  const { account, provider } = useWeb3React()
   const [connectedAccount, setConnected] = useAtom(walletAtom)
+  console.log('provider', provider)
 
   // TODO: Perfect place to program wallet popups
   useEffect(() => {
@@ -13,6 +28,11 @@ const WalletUpdater = () => {
       setConnected(account ?? '')
     }
   }, [account])
+
+  useEffect(() => {
+    connect(network)
+    connect(metaMask)
+  }, [])
 
   return null
 }
