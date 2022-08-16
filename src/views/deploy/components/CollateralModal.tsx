ad597a235bdc1ce1724de88ca0cb57ab420ee3e5
Luis Camargo <luiska1993@gmail.com>
2022-08-15 19:06:58 -0500

    chore: decouple custom collateral

--- src/views/deploy/components/CollateralModal.tsx
+++ src/views/deploy/components/CollateralModal.tsx
@@ -1,17 +1,9 @@
-import { Web3Provider } from '@ethersproject/providers'
 import { t, Trans } from '@lingui/macro'
-import { useWeb3React } from '@web3-react/core'
-import { CollateralInterface, ERC20Interface } from 'abis'
-import { Button, Input, Modal } from 'components'
-import { SmallButton } from 'components/button'
+import { Button, Modal } from 'components'
 import { ModalProps } from 'components/modal'
-import { ethers } from 'ethers'
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
-import { useCallback, useState } from 'react'
-import { promiseMulticall } from 'state/web3/lib/multicall'
-import { Box, Divider, Flex, Text } from 'theme-ui'
-import { isAddress } from 'utils'
-import { ZERO_ADDRESS } from 'utils/addresses'
+import { useState } from 'react'
+import { Box, Divider, Text } from 'theme-ui'
 import {
   addBackupCollateralAtom,
   addBasketCollateralAtom,
@@ -20,6 +12,7 @@ import {
   primaryBasketCollateralAtom,
 } from '../atoms'
 import collateralPlugins, { CollateralPlugin } from '../plugins'
+import CustomCollateral from './CustomCollateral'
 import PluginItem from './PluginItem'
 
 interface Props extends Omit<ModalProps, 'children'> {
@@ -44,144 +37,6 @@ const getPlugins = (addedCollaterals: string[], targetUnit?: string) => {
     }
     return acc
   }, {} as CollateralMap)
-}
-
-const CustomCollateral = ({
-  onAdd,
-}: {
-  onAdd(collateral: CollateralPlugin): void
-}) => {
-  const [isActive, setActive] = useState(false)
-  const [isValidating, setValidating] = useState(false)
-  const [address, setAddress] = useState('')
-  const [error, setError] = useState('')
-  const { provider } = useWeb3React()
-
-  const validatePlugin = useCallback(
-    async (address: string, provider: Web3Provider) => {
-      try {
-        setValidating(true)
-        const callParams = {
-          abi: CollateralInterface,
-          address,
-          args: [],
-        }
-
-        const [isCollateral, targetUnit, erc20, rewardERC20] =
-          await promiseMulticall(
-            [
-              { ...callParams, method: 'isCollateral' },
-              { ...callParams, method: 'targetName' },
-              { ...callParams, method: 'erc20' },
-              { ...callParams, method: 'rewardERC20' },
-            ],
-            provider
-          )
-
-        if (!isCollateral) {
-          throw new Error('INVALID COLLATERAL')
-        }
-
-        const metaCalls = [
-          { abi: ERC20Interface, args: [], address: erc20, method: 'symbol' },
-          { abi: ERC20Interface, args: [], address: erc20, method: 'decimals' },
-        ]
-
-        const [symbol, decimals] = await promiseMulticall(metaCalls, provider)
-
-        const collateral: CollateralPlugin = {
-          symbol,
-          address,
-          decimals,
-          targetUnit: ethers.utils.parseBytes32String(targetUnit),
-          referenceUnit: symbol,
-          collateralToken: symbol,
-          description: '',
-          collateralAddress: erc20,
-          rewardToken: rewardERC20 || ZERO_ADDRESS,
-          custom: true,
-        }
-
-        setValidating(false)
-        setAddress('')
-        onAdd(collateral)
-      } catch (e) {
-        console.error('Error validating collateral plugin', e)
-        setValidating(false)
-        setError(t`Invalid collateral`)
-      }
-    },
-    []
-  )
-
-  const handleChange = (value: string) => {
-    const parsedAddress = isAddress(value)
-    setAddress(parsedAddress || value)
-    if (!parsedAddress && value) {
-      setError(t`Invalid address`)
-    } else if (error) {
-      setError('')
-    }
-  }
-
-  const handleAdd = () => {
-    if (!provider) {
-      // TODO: Show error
-    } else {
-      validatePlugin(address, provider)
-    }
-  }
-
-  if (isActive) {
-    return (
-      <Box pt={2}>
-        <Box>
-          <Text variant="legend" ml={2}>
-            Plugin address
-          </Text>
-          <Input
-            mt={2}
-            onChange={handleChange}
-            value={address}
-            placeholder={t`Input plugin address`}
-          />
-          {error && address && (
-            <Text sx={{ color: 'danger' }} ml={2}>
-              {error}
-            </Text>
-          )}
-        </Box>
-        <Flex mt={3}>
-          <SmallButton variant="muted" onClick={() => setActive(false)}>
-            <Trans>Dismiss</Trans>
-          </SmallButton>
-          <SmallButton
-            ml="auto"
-            disabled={!!error || isValidating}
-            onClick={handleAdd}
-          >
-            {isValidating ? <Trans>Validating...</Trans> : <Trans>Save</Trans>}
-          </SmallButton>
-        </Flex>
-      </Box>
-    )
-  }
-
-  return (
-    <Flex variant="layout.verticalAlign" pt={2}>
-      <Box>
-        <Text>
-          <Trans>Made your own collateral?</Trans>
-        </Text>
-        <Text variant="legend" mt={1} sx={{ fontSize: 1, display: 'block' }}>
-          <Trans>Use a custom plugin contract address</Trans>
-        </Text>
-      </Box>
-      <SmallButton ml="auto" variant="muted" onClick={() => setActive(true)}>
-        <Trans>Add</Trans>
-      </SmallButton>
-    </Flex>
-  )
 }
 
 /**
