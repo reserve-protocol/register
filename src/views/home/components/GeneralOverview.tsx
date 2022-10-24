7693f199e2ff0056adb6cb2c3b263fd9039944be
Luis Camargo <luiska1993@gmail.com>
2022-10-24 12:55:55 -0500

    chore: update translations

--- src/views/home/components/GeneralOverview.tsx
+++ src/views/home/components/GeneralOverview.tsx
@@ -6,7 +6,7 @@ import useDebounce from 'hooks/useDebounce'
 import useQuery from 'hooks/useQuery'
 import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
-import { blockTimestampAtom, rpayTransactionsAtom } from 'state/atoms'
+import { rpayTransactionsAtom } from 'state/atoms'
 import RSVTxListener from 'state/RSVTxListener'
 import { BoxProps } from 'theme-ui'
 
