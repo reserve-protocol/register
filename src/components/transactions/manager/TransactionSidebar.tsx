559f05e74df094270f2ccd7ba3544f3c88aa3adf
wistec-bisrum <erik.davis@reserve.org>
2022-11-08 00:46:23 +0100

    add border and shadow to the tx sidebar
    I had to go from a flex component to box component for theme variables to not completely break the view. Not sure what's going on there.

--- src/components/transactions/manager/TransactionSidebar.tsx
+++ src/components/transactions/manager/TransactionSidebar.tsx
@@ -23,21 +23,24 @@ const Container = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
           backgroundColor: theme.colors.modalOverlay,
         })}
       />
-      <Flex
-        sx={{
+      <Box
+        sx={(theme: any) => ({
           flexDirection: 'column',
           zIndex: 100001,
+          display: 'flex',
           position: 'absolute',
           maxWidth: ['100vw', '768px'],
           width: ['100vw', '100vw', '60vw'],
           backgroundColor: 'background',
           right: 0,
+          borderLeft: `solid 2px ${theme.colors.darkBorder}`,
+          boxShadow: '-32px 0px 64px rgba(0, 0, 0, 0.15)',
           top: 0,
           height: '100vh',
-        }}
+        })}
       >
         {children}
-      </Flex>
+      </Box>
     </Portal>
   )
 }
