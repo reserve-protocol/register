a05e19ac1ccacd1f8235dc2cd9f908ccaaf82e85
Luis Camargo <luiska1993@gmail.com>
2022-11-10 15:12:06 -0600

    chore: basic layout

--- src/views/governance/index.tsx
+++ src/views/governance/index.tsx
@@ -1,23 +1,42 @@
 import { Trans, t } from '@lingui/macro'
 import { useMemo } from 'react'
-import { Box, Card, Grid, Text } from 'theme-ui'
+import { Box, BoxProps, Card, Grid, Text } from 'theme-ui'
 
-const Navigation = () => {
+const Navigation = (props: BoxProps) => {
   const items = useMemo(
     () => [
       { label: t`Intro` },
-      { label: t`Primary Basket` },
-      { label: t`Emergency Basket` },
+      { label: t`Primary basket` },
+      { label: t`Emergency basket` },
+      { label: t`RToken params` },
+      { label: t`Governance params` },
+      { label: t`Roles` },
     ],
     []
   )
 
   return (
-    <Box>
+    <Box {...props}>
       <Text variant="title">
         <Trans>Navigation</Trans>
       </Text>
-      <Box></Box>
+      <Box as="ul" mt={5} mr={3} p={0} sx={{ listStyle: 'none' }}>
+        {items.map((item) => (
+          <Box
+            as="li"
+            pl={4}
+            mb={4}
+            sx={{
+              lineHeight: '24px',
+              borderLeft: '3px solid',
+              borderColor: 'text',
+              cursor: 'pointer',
+            }}
+          >
+            <Text>{item.label}</Text>
+          </Box>
+        ))}
+      </Box>
     </Box>
   )
 }
@@ -32,8 +51,12 @@ const Proposal = () => {
 
 const Governance = () => {
   return (
-    <Grid columns={['1fr 1fr 1fr']} padding={[4, 5]}>
-      <Navigation />
+    <Grid
+      columns={['1fr', '1fr 1fr', '1fr 1fr', 'auto 1fr 420px']}
+      gap={4}
+      padding={[4, 5]}
+    >
+      <Navigation sx={{ display: ['none', 'none', 'none', 'inherit'] }} />
       <Proposal />
       <Summary />
     </Grid>
