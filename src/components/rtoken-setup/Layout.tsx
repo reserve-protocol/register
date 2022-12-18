16e86c72c597ccdf8dd24af51d2e419263ec82fd
wistec-bisrum <erik.davis@reserve.org>
2022-12-18 23:01:20 +0100

    widen width of navigation column

--- src/components/rtoken-setup/Layout.tsx
+++ src/components/rtoken-setup/Layout.tsx
@@ -3,9 +3,9 @@ import { BoxProps, Grid } from 'theme-ui'
 const Layout = ({ children, ...props }: BoxProps) => (
   <Grid
     id="rtoken-setup-container"
-    columns={['1fr', '1fr 1fr', '1.5fr 1fr', 'auto 1fr 420px']}
-    gap={4}
-    px={[4, 5]}
+    columns={['1fr', '1fr 1fr', '1.5fr 1fr', '200px 1fr 420px']}
+    gap={5}
+    px={[4, 7]}
     pt={[4, 5]}
     sx={{
       height: '100%',
