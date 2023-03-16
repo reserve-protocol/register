dc4cd49e46702a6f032576d47a7e8d0252b12b83
Luis Camargo <luiska1993@gmail.com>
2023-03-15 18:57:19 -0600

    feat: handle proposal actions

--- src/views/governance/views/proposal-detail/components/ProposalExecute.tsx
+++ src/views/governance/views/proposal-detail/components/ProposalExecute.tsx
@@ -1,7 +1,68 @@
-import { Box } from 'theme-ui'
+import { t } from '@lingui/macro'
+import { useWeb3React } from '@web3-react/core'
+import { LoadingButton } from 'components/button'
+import { ethers } from 'ethers'
+import { useAtomValue, useSetAtom } from 'jotai'
+import { useEffect, useState } from 'react'
+import { addTransactionAtom, rTokenGovernanceAtom } from 'state/atoms'
+import { useTransaction } from 'state/web3/hooks/useTransactions'
+import { TRANSACTION_STATUS } from 'utils/constants'
+import { v4 as uuid } from 'uuid'
+import { proposalDetailAtom } from '../atom'
 
 const ProposalExecute = () => {
-  return <Box>queue</Box>
+  const { account } = useWeb3React()
+  const governance = useAtomValue(rTokenGovernanceAtom)
+  const addTransaction = useSetAtom(addTransactionAtom)
+  const [txId, setTx] = useState('')
+  const proposal = useAtomValue(proposalDetailAtom)
+  const tx = useTransaction(txId)
+
+  useEffect(() => {
+    if (
+      tx?.status === TRANSACTION_STATUS.CONFIRMED ||
+      tx?.status === TRANSACTION_STATUS.REJECTED
+    ) {
+      setTx('')
+    }
+  }, [tx?.status])
+
+  const handleExecute = () => {
+    if (account && governance.governor && proposal) {
+      const id = uuid()
+      setTx(id)
+      addTransaction([
+        {
+          id,
+          description: t`Execute proposal`,
+          status: TRANSACTION_STATUS.PENDING,
+          value: '0',
+          call: {
+            abi: 'governance',
+            address: governance.governor,
+            method: 'execute',
+            args: [
+              proposal.targets,
+              new Array(proposal.targets.length).fill(0),
+              proposal.calldatas,
+              ethers.utils.formatBytes32String(proposal.description),
+            ],
+          },
+        },
+      ])
+    }
+  }
+
+  return (
+    <LoadingButton
+      small
+      loading={!!txId}
+      ml="auto"
+      disabled={!account}
+      onClick={handleExecute}
+      text={t`Execute proposal`}
+    />
+  )
 }
 
 export default ProposalExecute
--- src/views/governance/views/proposal-detail/index.tsx
+++ src/views/governance/views/proposal-detail/index.tsx
@@ -1,12 +1,8 @@
 import { Trans } from '@lingui/macro'
-import MDEditor from '@uiw/react-md-editor'
 import { useWeb3React } from '@web3-react/core'
 import { GovernanceInterface } from 'abis'
 import { SmallButton } from 'components/button'
-import GoTo from 'components/button/GoTo'
-import dayjs from 'dayjs'
-import { BigNumber } from 'ethers'
-import { formatEther, parseEther } from 'ethers/lib/utils'
+import { formatEther } from 'ethers/lib/utils'
 import useBlockNumber from 'hooks/useBlockNumber'
 import useRToken from 'hooks/useRToken'
 import { useAtomValue, useSetAtom } from 'jotai'
@@ -15,64 +11,18 @@ import { ArrowLeft } from 'react-feather'
 import { useNavigate, useParams } from 'react-router-dom'
 import { rTokenGovernanceAtom } from 'state/atoms'
 import { promiseMulticall } from 'state/web3/lib/multicall'
-import { Box, Card, Divider, Grid, Image, Text } from 'theme-ui'
-import { parseDuration, shortenAddress } from 'utils'
+import { Box, Grid } from 'theme-ui'
 import { CHAIN_ID } from 'utils/chains'
 import { PROPOSAL_STATES, ROUTES } from 'utils/constants'
-import { ExplorerDataType, getExplorerLink } from 'utils/getExplorerLink'
-import ProposalDetail from 'views/governance/components/ProposalDetailPreview'
-import {
-  accountVotesAtom,
-  getProposalStateAtom,
-  proposalDetailAtom,
-} from './atom'
-import ProposalAction from './components/ProposalAction'
+import { accountVotesAtom, proposalDetailAtom } from './atom'
+import ProposalAlert from './components/ProposalAlert'
+import ProposalDetailContent from './components/ProposalDetailContent'
 import ProposalDetailStats from './components/ProposalDetailStats'
+import ProposalExecute from './components/ProposalExecute'
+import ProposalQueue from './components/ProposalQueue'
+import ProposalVote from './components/ProposalVote'
 import ProposalVotes from './components/ProposalVotes'
 import useProposalDetail from './useProposalDetail'
-
-const ProposalAlert = () => {
-  const state = useAtomValue(getProposalStateAtom)
-
-  if (!state.deadline) {
-    return null
-  }
-
-  const deadline = parseDuration(state.deadline, {
-    units: ['d', 'h', 'm'],
-    round: true,
-  })
-
-  return (
-    <Box
-      ml="auto"
-      py={2}
-      px={3}
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        background: 'infoBG',
-        borderRadius: '30px',
-        color: 'info',
-        fontSize: 0,
-      }}
-    >
-      <Image src="/svgs/asterisk.svg" />
-      <Text ml={2} mr="1" sx={{ fontWeight: 500 }}>
-        {state.state === PROPOSAL_STATES.ACTIVE && (
-          <Trans>Voting ends in:</Trans>
-        )}
-        {state.state === PROPOSAL_STATES.PENDING && (
-          <Trans>Voting starts in:</Trans>
-        )}
-        {state.state === PROPOSAL_STATES.QUEUED && (
-          <Trans>Execution delay ends in:</Trans>
-        )}
-      </Text>
-      {deadline}
-    </Box>
-  )
-}
 
 const GovernanceProposalDetail = () => {
   const { proposalId } = useParams()
@@ -84,15 +34,6 @@ const GovernanceProposalDetail = () => {
   const setAccountVoting = useSetAtom(accountVotesAtom)
   const blockNumber = useBlockNumber()
   const navigate = useNavigate()
-
-  let title = 'Loading...'
-  let description = ''
-
-  if (proposal?.description) {
-    const [heading, ...content] = proposal.description.split(/\r?\n/)
-    title = heading.replaceAll('#', '').trim()
-    description = content.join('\n')
-  }
 
   const handleBack = () => {
     navigate(`${ROUTES.GOVERNANCE}?token=${rToken?.address}`)
@@ -184,55 +125,14 @@ const GovernanceProposalDetail = () => {
             </Box>
           </SmallButton>
           <ProposalAlert />
+          {proposal?.state === PROPOSAL_STATES.SUCCEEDED && <ProposalQueue />}
+          {proposal?.state === PROPOSAL_STATES.QUEUED && <ProposalExecute />}
         </Box>
-
-        <Card p={4} mb={4}>
-          <Text variant="title" mb={2}>
-            {title}
-          </Text>
-          <Box variant="layout.verticalAlign" sx={{ fontSize: 1 }}>
-            <Text variant="legend" mr={1}>
-              <Trans>Proposed by</Trans>:
-            </Text>
-            <Text>
-              {proposal?.proposer
-                ? shortenAddress(proposal.proposer)
-                : 'Loading...'}
-            </Text>
-            {!!proposal?.proposer && (
-              <GoTo
-                ml={1}
-                href={getExplorerLink(
-                  proposal.proposer,
-                  ExplorerDataType.ADDRESS
-                )}
-              />
-            )}
-
-            <Text variant="legend" ml={3} mr={1}>
-              <Trans>Proposed on</Trans>:
-            </Text>
-            <Text>
-              {proposal?.creationTime
-                ? dayjs.unix(+proposal.creationTime).format('MMM D, YYYY')
-                : 'Loading...'}
-            </Text>
-          </Box>
-          <Divider my={3} mx={-4} sx={{ borderColor: 'darkBorder' }} />
-          <MDEditor.Markdown
-            source={description}
-            style={{ whiteSpace: 'pre-wrap', backgroundColor: 'transparent' }}
-          />
-        </Card>
-        {!!proposal && (
-          <ProposalDetail
-            addresses={proposal.targets}
-            calldatas={proposal.calldatas}
-          />
-        )}
+        <ProposalDetailContent />
       </Box>
       <Box>
-        <ProposalAction />
+        {(proposal?.state === PROPOSAL_STATES.PENDING ||
+          proposal?.state === PROPOSAL_STATES.ACTIVE) && <ProposalVote />}
         <ProposalDetailStats />
         <ProposalVotes />
       </Box>
