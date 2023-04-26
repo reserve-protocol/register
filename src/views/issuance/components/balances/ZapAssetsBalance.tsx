731a1ce27eb47b3b5db89f117be1da0d808d2a08
Luis Camargo <luiska1993@gmail.com>
2023-04-26 16:24:41 -0600

    chore: choose zap token from balances

---
+++ src/views/issuance/components/balances/ZapAssetsBalance.tsx
@@ -0,0 +1,63 @@
+import { Trans } from '@lingui/macro'
+import { Token } from '@reserve-protocol/token-zapper/types/entities'
+import TokenBalance from 'components/token-balance'
+import { formatUnits } from 'ethers/lib/utils'
+import { atom, useAtomValue, useSetAtom } from 'jotai'
+import { tokenBalancesStore } from 'state/TokenBalancesUpdater'
+import { Box, Text } from 'theme-ui'
+import { BI_ZERO } from 'utils/constants'
+import { selectedZapTokenAtom } from '../zap/state/atoms'
+import { ui } from '../zap/state/ui-atoms'
+
+const zapTokenBalancesAtom = atom((get) => {
+  const tokens = get(ui.input.tokenSelector.tokenSelector) || []
+
+  return tokens.reduce((acc, token) => {
+    const value = formatUnits(
+      get(tokenBalancesStore.getBalanceAtom(token.address.address)).value ||
+        BI_ZERO,
+      token.decimals
+    )
+
+    acc[token.address.address] = value
+
+    return acc
+  }, {} as { [x: string]: string })
+})
+
+const ZapAssetsBalances = () => {
+  const tokens = useAtomValue(ui.input.tokenSelector.tokenSelector) || []
+  const selectedToken = useAtomValue(selectedZapTokenAtom)
+  const balances = useAtomValue(zapTokenBalancesAtom)
+  const setZapToken = useSetAtom(ui.input.tokenSelector.tokenSelector)
+
+  return (
+    <Box>
+      <Text variant="subtitle" mb={3} p={4} pb={0}>
+        <Trans>Available assets</Trans>
+      </Text>
+      <Box sx={{ overflow: 'auto', maxHeight: 360 }} p={4} pt={0}>
+        {tokens.map((token) => {
+          const isSelected =
+            selectedToken?.address.address === token.address.address
+
+          return (
+            <TokenBalance
+              key={token.address.address}
+              mb={2}
+              symbol={token.symbol}
+              balance={+balances[token.address.address] || 0}
+              onClick={() => setZapToken(token)}
+              sx={{
+                cursor: isSelected ? 'inherit' : 'pointer',
+                opacity: isSelected ? 1 : 0.5,
+              }}
+            />
+          )
+        })}
+      </Box>
+    </Box>
+  )
+}
+
+export default ZapAssetsBalances
