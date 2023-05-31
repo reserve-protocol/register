0abd66eb460452d2c20ce0f033b682c09edf2bca
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-05-31 02:17:26 +0200

    chore: styling in appHeader and the tokenSelector dropdown

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -5,8 +5,9 @@ import Popover, { PopoverProps } from 'components/popover'
 const Container = styled(Box)`
   background-color: ${({ theme }: { theme: any }) =>
     theme.colors.contentBackground};
-  border: 2px solid var(--theme-ui-colors-border);
-  border-radius: 10px;
+  border: 1px solid var(--theme-ui-colors-invertedText);
+  border-radius: 14px;
+  box-shadow: 0px 10px 25px rgba(0, 0, 0, 0.2);
 `
 
 const Popup = ({ content, ...props }: PopoverProps) => {
