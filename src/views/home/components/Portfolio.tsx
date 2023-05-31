65a5c71602876494a3793ae00ebbf348c4d56ec7
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-05-31 02:37:20 +0200

    chore: improve styling for portfolio section

--- src/views/home/components/Portfolio.tsx
+++ src/views/home/components/Portfolio.tsx
@@ -10,7 +10,7 @@ import {
   accountPositionsAtom,
   accountTokensAtom,
 } from 'state/atoms'
-import { Box, BoxProps, Divider, Text } from 'theme-ui'
+import { Box, Flex, BoxProps, Divider, Text } from 'theme-ui'
 import {
   formatCurrency,
   formatCurrencyCell,
@@ -88,48 +88,59 @@ const Portfolio = (props: BoxProps) => {
   }
 
   return (
-    <Box {...props}>
-      <Box ml={3} mt={2}>
-        <Text variant="sectionTitle">
-          <Trans>Staked RSR + Rtoken Value</Trans>
-        </Text>
-        <Text
-          mt={0}
-          pt={0}
-          sx={{ fontSize: [4, 7], fontWeight: 400, color: 'boldText' }}
-          as="h1"
-        >
-          ${formatCurrency(holdings)}
-        </Text>
+    <Box
+      {...props} /* sx={{border: '1px dashed',  borderColor: 'primary', borderRadius: 12}}*/
+    >
+      <Box>
+        <Box ml={3}>
+          <Text mb={1} variant="title" sx={{ color: 'lightText' }}>
+            <Trans>Staked RSR + RToken Value</Trans>
+          </Text>
+          <Text
+            ml={[0, '-1px']}
+            sx={{ fontSize: [4, 7], fontWeight: 400, color: 'text' }}
+            as="h1"
+          >
+            ${formatCurrency(holdings)}
+          </Text>
+        </Box>
+        {rTokens?.length > 0 && (
+          <Box mt={5}>
+            <Text
+              pl={3}
+              variant="title"
+              sx={{ color: 'secondaryText', fontWeight: '400' }}
+            >
+              <Trans>Your RTokens</Trans>
+            </Text>
+            <Table
+              mt={3}
+              maxHeight={220}
+              columns={rTokenColumns}
+              data={rTokens}
+            />
+          </Box>
+        )}
+        {stTokens?.length > 0 && (
+          <Box mt={5}>
+            <Text
+              pl={3}
+              variant="title"
+              sx={{ color: 'secondaryText', fontWeight: '400' }}
+            >
+              <Trans>Your staked RSR positions</Trans>
+            </Text>
+            <Table
+              mt={2}
+              mb="-16px"
+              maxHeight={220}
+              columns={stTokenColumns}
+              data={stTokens}
+            />
+          </Box>
+        )}
       </Box>
-      {rTokens?.length > 0 && (
-        <Box mt={5}>
-          <Text pl={3} variant="title">
-            <Trans>Your RTokens</Trans>
-          </Text>
-          <Table
-            mt={3}
-            maxHeight={220}
-            columns={rTokenColumns}
-            data={rTokens}
-          />
-        </Box>
-      )}
-      {stTokens?.length > 0 && (
-        <Box mt={5}>
-          <Text pl={3} variant="title">
-            <Trans>Your staked RSR positions</Trans>
-          </Text>
-          <Table
-            mt={3}
-            mb="-16px"
-            maxHeight={220}
-            columns={stTokenColumns}
-            data={stTokens}
-          />
-        </Box>
-      )}
-      <Divider mx={[-4, -5]} my={6} sx={{ borderColor: 'darkBorder' }} />
+      <Divider mx={[-1, -5]} my={[6, 8]} />
     </Box>
   )
 }
