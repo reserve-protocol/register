f809e4a8d22d3a8a0213c05d36a20e4a55ec0215
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-06-05 19:06:16 +0200

    chore: add icons for revenue distribution types

---
+++ src/components/icons/RevenueToExternalIcon.tsx
@@ -0,0 +1,15 @@
+import { SVGProps } from 'react'
+
+const RevenueToExternalIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="3" y="3" width="10" height="10" rx="5" fill="#333333" />
+  </svg>
+)
+
+export default RevenueToExternalIcon
