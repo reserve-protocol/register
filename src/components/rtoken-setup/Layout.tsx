94652e6918b4591fde76ca63f74b4ecf59624ac0
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-06-15 00:22:20 +0200

    chore: slight change in right rtoken setup column width

--- src/components/rtoken-setup/Layout.tsx
+++ src/components/rtoken-setup/Layout.tsx
@@ -3,9 +3,9 @@ import { BoxProps, Grid } from 'theme-ui'
 const Layout = ({ children, ...props }: BoxProps) => (
   <Grid
     id="rtoken-setup-container"
-    columns={['1fr', '1fr 1fr', '1.5fr 1fr', '200px 1fr 400px']}
+    columns={['1fr', '1fr 1fr', '1.5fr 1fr', '200px 1fr 440px']}
     gap={5}
-    px={[2, 4, 7]}
+    px={[2, 4, 5]}
     pt={[2, 4, 6]}
     sx={{
       height: '100%',
