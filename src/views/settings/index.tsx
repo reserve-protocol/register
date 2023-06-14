21980872b09e798b51be45d5056f0f352bda0ac3
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-06-14 19:29:19 +0200

    chore: don't collapse nav in settings view

--- src/views/settings/index.tsx
+++ src/views/settings/index.tsx
@@ -1,12 +1,9 @@
 import Layout from 'components/rtoken-setup/Layout'
-import useToggledSidebar from 'hooks/useToggledSidebar'
 import NavigationSidebar from './components/NavigationSidebar'
 import RTokenManagement from './components/RTokenManagement'
 import RTokenOverview from './components/RTokenOverview'
 
 const Settings = () => {
-  useToggledSidebar()
-
   return (
     <Layout>
       <NavigationSidebar />
