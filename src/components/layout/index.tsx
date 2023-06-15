b831a6d92545c0f52a7a72f54785769d3bf0bda9
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-06-15 03:20:20 +0200

    chore: add sidebar footer to bottom of page (excluding caution warning)

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,7 +4,7 @@ import { Box, Flex } from 'theme-ui'
 import Header from './header'
 import MobileNav from './navigation/MobileNav'
 import Sidebar from './sidebar'
-
+import Footer from './sidebar/Footer'
 const Container = styled(Flex)`
   height: 100%;
   max-width: 95em;
@@ -51,7 +51,9 @@ const Layout = ({ children }: { children: ReactNode }) => (
       <Sidebar />
       <Body>
         <Suspense>
-          <ContentContainer id="app-container">{children}</ContentContainer>
+          <ContentContainer id="app-container">
+            {children} <Footer />
+          </ContentContainer>
         </Suspense>
       </Body>
       <MobileNav />
--- src/components/layout/sidebar/Footer.tsx
+++ src/components/layout/sidebar/Footer.tsx
@@ -1,40 +1,11 @@
-import { Trans } from '@lingui/macro'
 import SyncedBlock from 'components/synced-block'
-import Help from 'components/help'
-import { AlertCircle } from 'react-feather'
 import { Box, BoxProps, Flex, Link, Text, Card } from 'theme-ui'
 
 const Footer = (props: BoxProps) => (
-  <Box m={4} {...props}>
-    <Box sx={{ fontSize: 1 }} mb={4}>
-      <Card
-        mx={-2}
-        py={2}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          background: 'rgba(255, 138, 0, 0.1)',
-          borderRadius: '8px',
-          color: 'warning',
-        }}
-      >
-        <AlertCircle size={16} />
-        <Text ml={2} mr="auto" sx={{ fontWeight: 500 }}>
-          <Trans>Proceed with caution</Trans>
-        </Text>
-        <Help content="Both Register & the Reserve Protocol are brand new. There are risks with using any new smart contract technology." />
-      </Card>
-    </Box>
+  <Box px={6} mb={8} mt={6} {...props}>
     <Flex sx={{ alignItems: 'center' }}>
       <Link target="_blank" href="https://github.com/lc-labs">
-        <Text
-          sx={{
-            fontSize: 0,
-          }}
-          variant="legend"
-        >
-          Made by LC Labs
-        </Text>
+        <Text variant="smallLabel">Made by LC Labs</Text>
       </Link>
       <Box mx="auto" />
       <SyncedBlock />
