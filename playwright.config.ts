940dda33f52913749dfea05609f5f88f88932c9f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2023-06-21 23:58:55 +0700

    chore: setup guardian web3 test

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test'
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  testMatch: [/.*gui\.(js|ts|mjs)/],
+  timeout: 90 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+    headless: true,
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--disable-web-security'],
+        },
+      },
+    },
+  ],
+
+  webServer: {
+    command: 'yarn start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
+}
+
+export default config
---
+++ tests/mint.gui.ts
@@ -0,0 +1,7 @@
+import { test } from '@guardianui/test'
+
+test.describe('RToken minting', () => {
+  test('Should able to mint eUSD', async () => {
+    //
+  })
+})
