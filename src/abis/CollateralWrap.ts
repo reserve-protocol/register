fe23fa9bb00d192df7fbd0a50f2b6e993d88fcdd
Luis Camargo <luiska1993@gmail.com>
2023-09-27 16:48:12 -0600

    chore: fix minor base bugs

--- src/abis/CollateralWrap.ts
+++ src/abis/CollateralWrap.ts
@@ -154,4 +154,12 @@ export default [
     stateMutability: 'nonpayable',
     type: 'function',
   },
+  // COMPv3
+  {
+    inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
+    name: 'deposit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
 ] as const
