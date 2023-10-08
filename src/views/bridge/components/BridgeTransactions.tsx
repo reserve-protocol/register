d27c62f2eaa325e5b40cb5cc9f25da643e26ce4b
Luis Camargo <luiska1993@gmail.com>
2023-10-07 19:44:26 -0600

    chore: handle basket is not ready state

--- src/views/bridge/components/BridgeTransactions.tsx
+++ src/views/bridge/components/BridgeTransactions.tsx
@@ -1,19 +1,28 @@
 import { useAtomValue } from 'jotai'
 import { walletAtom } from 'state/atoms'
 import useSWR from 'swr'
-import { Box, Text } from 'theme-ui'
+import { Box, Link, Text } from 'theme-ui'
 import useBridgeTransactions from '../hooks/useBridgeTransactions'
 
 const Transactions = () => {
-  const account = useAtomValue(walletAtom)
-  const txs = useBridgeTransactions()
-
-  console.log('txs', txs)
+  // const account = useAtomValue(walletAtom)
+  // const txs = useBridgeTransactions()
 
   return (
-    <Box>
-      <Text>Transactions</Text>
-      <Box></Box>
+    <Box
+      p={4}
+      sx={{ borderRadius: 14, border: '2px dashed', borderColor: 'warning' }}
+    >
+      <Text sx={{ color: 'warning' }} variant="strong">
+        Under development
+      </Text>
+      <Text>
+        This feature is not ready yet, you can process your withdrawals on the
+      </Text>{' '}
+      <Link href="https://bridge.base.org/transactions" target="_blank">
+        Base Official Bridge
+      </Link>{' '}
+      <Text>RSR withdrawals will appear as "Unlisted"</Text>
     </Box>
   )
 }
