b454ca5c7b24dd310bf2a7bcafb2693802d64cd6
Jan <mig@jankjr.dk>
2023-10-09 20:31:42 +0200

    add a small network marker to token list dropdown

--- src/components/token-item/index.tsx
+++ src/components/token-item/index.tsx
@@ -1,16 +1,33 @@
 import TokenLogo from 'components/icons/TokenLogo'
+import Base from 'components/icons/logos/Base'
+import Ethereum from 'components/icons/logos/Ethereum'
 import { Text, BoxProps, Box } from 'theme-ui'
 
 interface Props extends BoxProps {
   symbol: string
   logo?: string
   width?: number
+  chainId: number | null
 }
 
-const TokenItem = ({ symbol, logo, width = 20, ...props }: Props) => (
-  <Box variant="layout.verticalAlign">
-    <TokenLogo width={24} mr={2} symbol={symbol} src={logo} />
-    <Text {...props}>{symbol}</Text>
+const TokenItem = ({ symbol, logo, width = 20, chainId, ...props }: Props) => (
+  <Box
+    variant="layout.verticalAlign"
+    sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      width: "100%",
+      justifyContent: "space-between"
+    }}
+  >
+    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+      <TokenLogo width={24} mr={2} symbol={symbol} src={logo} />
+      <Text {...props}>{symbol}</Text>
+    </Box>
+    <Box>
+      {chainId === 1 && <Ethereum />}
+      {chainId === 8453 && <Base />}
+    </Box>
   </Box>
 )
 
