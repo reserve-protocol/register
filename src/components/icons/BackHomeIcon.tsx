6cd8858b055c890e3a34119583940b8b457028d3
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2023-10-12 20:20:09 +0200

    chore: replace dashboard icon

--- src/components/icons/BackHomeIcon.tsx
+++ src/components/icons/BackHomeIcon.tsx
@@ -8,12 +8,35 @@ const BackHomeIcon = (props: SVGProps<SVGSVGElement>) => (
     fill="none"
     {...props}
   >
-    <path
-      fill="currentColor"
-      fillRule="evenodd"
-      d="M4.355 19.1V9.092L12 3.146l7.645 5.946V19.1a.995.995 0 0 1-.995.995h-3.801V11.5h-5.7v8.595H5.35a.995.995 0 0 1-.995-.995ZM9.149 21H18.65a1.9 1.9 0 0 0 1.9-1.9V8.65L12 2 3.45 8.65V19.1a1.9 1.9 0 0 0 1.9 1.9h3.8Zm4.795-.905h-3.89v-7.69h3.89v7.69Z"
-      clipRule="evenodd"
-    />
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="5.17241"
+        cy="5.17241"
+        r="2.17241"
+        fill="#878787"
+        fill-opacity="0.5"
+      />
+      <circle cx="12.0001" cy="5.17241" r="2.17241" fill="currentColor" />
+      <circle
+        cx="18.8277"
+        cy="5.17241"
+        r="2.17241"
+        fill="#878787"
+        fill-opacity="0.5"
+      />
+      <circle cx="5.17241" cy="11.9999" r="2.17241" fill="currentColor" />
+      <circle cx="12.0001" cy="11.9999" r="2.17241" fill="#E7AC3B" />
+      <circle cx="18.8277" cy="11.9999" r="2.17241" fill="currentColor" />
+      <circle cx="5.17241" cy="18.8277" r="2.17241" fill="currentColor" />
+      <circle cx="12.0001" cy="18.8277" r="2.17241" fill="#92C5FA" />
+      <circle cx="18.8277" cy="18.8277" r="2.17241" fill="currentColor" />
+    </svg>
   </svg>
 )
 
