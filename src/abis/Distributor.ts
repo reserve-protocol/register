224c293b956db71af86fb5bdb1d610d61f64e99c
Akshat Mittal <itsreallyakshat@gmail.com>
2023-10-13 19:32:39 +0530

    Revert "Feature/base hyusd zaps" (#217)

--- src/abis/Distributor.ts
+++ src/abis/Distributor.ts
@@ -34,7 +34,12 @@ export default [
   {
     anonymous: false,
     inputs: [
-      { indexed: true, internalType: 'address', name: 'dest', type: 'address' },
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'dest',
+        type: 'address',
+      },
       {
         indexed: false,
         internalType: 'uint16',
@@ -54,7 +59,12 @@ export default [
   {
     anonymous: false,
     inputs: [
-      { indexed: false, internalType: 'uint8', name: 'version', type: 'uint8' },
+      {
+        indexed: false,
+        internalType: 'uint8',
+        name: 'version',
+        type: 'uint8',
+      },
     ],
     name: 'Initialized',
     type: 'event',
@@ -75,7 +85,7 @@ export default [
         type: 'address',
       },
       {
-        indexed: false,
+        indexed: true,
         internalType: 'uint256',
         name: 'amount',
         type: 'uint256',
@@ -100,35 +110,54 @@ export default [
   {
     inputs: [],
     name: 'FURNACE',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'MAX_DESTINATIONS_ALLOWED',
-    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'ST_RSR',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [],
-    name: 'cacheComponents',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
     inputs: [
-      { internalType: 'contract IERC20', name: 'erc20', type: 'address' },
-      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      {
+        internalType: 'contract IERC20',
+        name: 'erc20',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
     ],
     name: 'distribute',
     outputs: [],
@@ -136,22 +165,48 @@ export default [
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'address', name: '', type: 'address' }],
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     name: 'distribution',
     outputs: [
-      { internalType: 'uint16', name: 'rTokenDist', type: 'uint16' },
-      { internalType: 'uint16', name: 'rsrDist', type: 'uint16' },
+      {
+        internalType: 'uint16',
+        name: 'rTokenDist',
+        type: 'uint16',
+      },
+      {
+        internalType: 'uint16',
+        name: 'rsrDist',
+        type: 'uint16',
+      },
     ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [
-      { internalType: 'contract IMain', name: 'main_', type: 'address' },
+      {
+        internalType: 'contract IMain',
+        name: 'main_',
+        type: 'address',
+      },
       {
         components: [
-          { internalType: 'uint16', name: 'rTokenDist', type: 'uint16' },
-          { internalType: 'uint16', name: 'rsrDist', type: 'uint16' },
+          {
+            internalType: 'uint16',
+            name: 'rTokenDist',
+            type: 'uint16',
+          },
+          {
+            internalType: 'uint16',
+            name: 'rsrDist',
+            type: 'uint16',
+          },
         ],
         internalType: 'struct RevenueShare',
         name: 'dist',
@@ -166,24 +221,48 @@ export default [
   {
     inputs: [],
     name: 'main',
-    outputs: [{ internalType: 'contract IMain', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'contract IMain',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'proxiableUUID',
-    outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
+    outputs: [
+      {
+        internalType: 'bytes32',
+        name: '',
+        type: 'bytes32',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [
-      { internalType: 'address', name: 'dest', type: 'address' },
+      {
+        internalType: 'address',
+        name: 'dest',
+        type: 'address',
+      },
       {
         components: [
-          { internalType: 'uint16', name: 'rTokenDist', type: 'uint16' },
-          { internalType: 'uint16', name: 'rsrDist', type: 'uint16' },
+          {
+            internalType: 'uint16',
+            name: 'rTokenDist',
+            type: 'uint16',
+          },
+          {
+            internalType: 'uint16',
+            name: 'rsrDist',
+            type: 'uint16',
+          },
         ],
         internalType: 'struct RevenueShare',
         name: 'share',
@@ -201,8 +280,16 @@ export default [
     outputs: [
       {
         components: [
-          { internalType: 'uint24', name: 'rTokenTotal', type: 'uint24' },
-          { internalType: 'uint24', name: 'rsrTotal', type: 'uint24' },
+          {
+            internalType: 'uint24',
+            name: 'rTokenTotal',
+            type: 'uint24',
+          },
+          {
+            internalType: 'uint24',
+            name: 'rsrTotal',
+            type: 'uint24',
+          },
         ],
         internalType: 'struct RevenueTotals',
         name: 'revTotals',
@@ -214,7 +301,11 @@ export default [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'newImplementation', type: 'address' },
+      {
+        internalType: 'address',
+        name: 'newImplementation',
+        type: 'address',
+      },
     ],
     name: 'upgradeTo',
     outputs: [],
@@ -223,8 +314,16 @@ export default [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'newImplementation', type: 'address' },
-      { internalType: 'bytes', name: 'data', type: 'bytes' },
+      {
+        internalType: 'address',
+        name: 'newImplementation',
+        type: 'address',
+      },
+      {
+        internalType: 'bytes',
+        name: 'data',
+        type: 'bytes',
+      },
     ],
     name: 'upgradeToAndCall',
     outputs: [],
@@ -234,7 +333,13 @@ export default [
   {
     inputs: [],
     name: 'version',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
     stateMutability: 'pure',
     type: 'function',
   },
