6060187aa5f286c7a6b91aedd74d3ca570752b77
Luis Camargo <luiska1993@gmail.com>
2023-12-07 10:25:46 -0600

    feat: bridge faq

--- src/views/bridge/index.tsx
+++ src/views/bridge/index.tsx
@@ -1,23 +1,23 @@
 import { Box, Divider, Flex } from 'theme-ui'
 import Bridge from './components/Bridge'
 import BridgeWithdrawals from './components/BridgeWithdrawals'
+import BridgeFaq from './components/BridgeFaq'
 
-const ChainBridge = () => {
-  return (
-    <>
-      <Flex
-        py={[1, 8]}
-        sx={{
-          backgroundColor: 'contentBackground',
-          justifyContent: 'center',
-          borderRadius: '0 0 20px 20px',
-        }}
-      >
-        <Bridge />
-      </Flex>
-      <BridgeWithdrawals />
-    </>
-  )
-}
+const ChainBridge = () => (
+  <>
+    <Flex
+      py={[1, 8]}
+      sx={{
+        backgroundColor: 'contentBackground',
+        justifyContent: 'center',
+        borderRadius: '0 0 20px 20px',
+      }}
+    >
+      <Bridge />
+    </Flex>
+    <BridgeWithdrawals />
+    <BridgeFaq />
+  </>
+)
 
 export default ChainBridge
