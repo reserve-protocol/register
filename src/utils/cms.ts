3f2a0cc070c67f98c09e32578b260bd7ab675978
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-01-19 12:54:25 -0300

    chore: add temporary cms file

---
+++ src/utils/cms.ts
@@ -0,0 +1,136 @@
+export default {
+  "collaterals": [
+    {
+      "symbol": "cUSDC-VAULT",
+      "chain": 1,
+      "project": "COMP",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "cUSDT-VAULT",
+      "chain": 1,
+      "project": "COMP",
+      "color": "#53AE94"
+    },
+    {
+      "symbol": "saUSDC",
+      "chain": 1,
+      "project": "AAVE",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "saUSDT",
+      "chain": 1,
+      "project": "AAVE",
+      "color": "#53AE94"
+    },
+    {
+      "symbol": "wstETH",
+      "chain": 1,
+      "project": "GENERIC",
+      "color": "#627EEA"
+    },
+    {
+      "symbol": "rETH",
+      "chain": 1,
+      "project": "GENERIC",
+      "color": "#627EEA"
+    },
+    {
+      "symbol": "stkcvxeUSD3CRV-f",
+      "chain": 1,
+      "project": "CONVEX",
+      "color": "#7f7f7f"
+    },
+    {
+      "symbol": "mrp-aUSDT",
+      "chain": 1,
+      "project": "MORPHO",
+      "color": "#53AE94"
+    },
+    {
+      "symbol": "sDAI",
+      "chain": 1,
+      "project": "SDR",
+      "color": "#EAAF50"
+    },
+    {
+      "symbol": "fUSDC",
+      "chain": 1,
+      "project": "GENERIC",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "wcUSDCv3",
+      "chain": 1,
+      "project": "COMPv3",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "mrp-aUSDC",
+      "chain": 1,
+      "project": "MORPHO",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "wcUSDCv3",
+      "chain": 8453,
+      "project": "COMPv3",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "wsgUSDbC",
+      "chain": 8453,
+      "project": "STARGATE",
+      "color": "#2775CA"
+    },
+    {
+      "symbol": "saBasUSDbC",
+      "chain": 8453,
+      "project": "AAVE",
+      "color": "#2775CA"
+    }
+  ],
+  "projects": [
+    {
+      "name": "AAVE",
+      "label": "AAVE V2 Tokens",
+      "color": "#a75ca3"
+    },
+    {
+      "name": "COMP",
+      "label": "Compound V2 Tokens",
+      "color": "#39c7b1"
+    },
+    {
+      "name": "COMPv3",
+      "label": "Compound V3 Tokens",
+      "color": "#39c7b1"
+    },
+    {
+      "name": "CONVEX",
+      "label": "Curve Convex LP Tokens",
+      "color": "#373737"
+    },
+    {
+      "name": "GENERIC",
+      "label": "Generic Tokens",
+      "color": "#CCCCCC"
+    },
+    {
+      "name": "MORPHO",
+      "label": "Morpho (AAVE) Tokens",
+      "color": "#0059FF"
+    },
+    {
+      "name": "SDR",
+      "label": "Savings DAI",
+      "color": "#7AC028"
+    },
+    {
+      "name": "STARGATE",
+      "label": "Stargate Tokens",
+      "color": "#666666"
+    }
+  ]
+}
