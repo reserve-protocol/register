329e6c565fffc26fa8bf12cde3bcd1ea6b6a0a4a
Luis Camargo <luiska1993@gmail.com>
2024-01-28 11:20:55 -0600

    chore: fix token logos

--- src/components/icons/TokenLogo.tsx
+++ src/components/icons/TokenLogo.tsx
@@ -23,6 +23,7 @@ const IMGS = new Set([
   'cusdt',
   'usdc',
   'cusdc',
+  'usdbc',
   'usdp',
   'wbtc',
   'cwbtc',
@@ -54,6 +55,7 @@ const IMGS = new Set([
   'mrp-asteth',
   'frax',
   'crvusd',
+  'mkusd',
 ])
 
 const TokenLogo = ({
