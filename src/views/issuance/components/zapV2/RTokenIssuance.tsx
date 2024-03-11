31aff728007b1760ffca6218fbea0fda74dbd1a9
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-03-11 10:01:55 -0300

    feat/wip: add input and output zaps components

--- src/views/issuance/components/zapV2/RTokenIssuance.tsx
+++ src/views/issuance/components/zapV2/RTokenIssuance.tsx
@@ -1,16 +1,17 @@
-import WalletOutlineIcon from 'components/icons/WalletOutlineIcon'
+import { NumericalInput } from 'components'
 import TabMenu from 'components/tab-menu'
 import { FC, useState } from 'react'
-import { ChevronRight, Minus, Plus, Settings } from 'react-feather'
-import { Box, Divider, IconButton, Text } from 'theme-ui'
+import { ArrowDown, Minus, Plus, Settings } from 'react-feather'
+import { Box, Button, Divider, IconButton, Text } from 'theme-ui'
 import { ReserveToken } from 'types'
+import { formatCurrency } from 'utils'
 
-type IssuanceTabsProps = {
+type ZapTabsProps = {
   symbol: string
 }
 
-const IssuanceTabs: FC<IssuanceTabsProps> = ({ symbol }) => {
-  const [issuanceOperation, setIssuanceOperation] = useState<string>('mint')
+const ZapTabs: FC<ZapTabsProps> = ({ symbol }) => {
+  const [issuanceOperation, setZapOperation] = useState<string>('mint')
   const backingOptions = [
     { key: 'mint', label: 'Mint', icon: <Plus size={16} /> },
     { key: 'redeem', label: 'Redeem', icon: <Minus size={16} /> },
@@ -21,65 +22,151 @@ const IssuanceTabs: FC<IssuanceTabsProps> = ({ symbol }) => {
       variant="layout.verticalAlign"
       sx={{ justifyContent: 'space-between' }}
     >
-      <Box variant="layout.verticalAlign" sx={{ gap: 2 }}>
-        <TabMenu
-          mt={[3, 0]}
-          active={issuanceOperation}
-          items={backingOptions}
-          small
-          background="border"
-          onMenuChange={setIssuanceOperation}
-        />
-        <IconButton
+      <TabMenu
+        mt={[3, 0]}
+        active={issuanceOperation}
+        items={backingOptions}
+        small
+        background="border"
+        onMenuChange={setZapOperation}
+      />
+      <IconButton
+        sx={{
+          cursor: 'pointer',
+          width: '34px',
+          border: '1px solid',
+          borderColor: 'border',
+          borderRadius: '6px',
+          ':hover': { backgroundColor: 'border' },
+        }}
+      >
+        <Settings size={16} />
+      </IconButton>
+    </Box>
+  )
+}
+
+type ZapInputProps = {
+  symbol: string
+  amount: string
+  setAmount: (amount: string) => void
+}
+
+const ZapInput: FC<ZapInputProps> = ({ symbol, amount, setAmount }) => {
+  return (
+    <Box sx={{ position: 'relative', zIndex: 0, width: '100%' }}>
+      <NumericalInput
+        variant="transparent"
+        placeholder={`0 ${symbol}`}
+        value={amount}
+        onChange={setAmount}
+      />
+      {!!amount && (
+        <Box
           sx={{
-            cursor: 'pointer',
-            width: '34px',
-            border: '1px solid',
-            borderColor: 'border',
-            borderRadius: '6px',
-            ':hover': { backgroundColor: 'border' },
+            fontSize: 4,
+            fontWeight: 'bold',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: -1,
           }}
         >
-          <Settings size={16} />
-        </IconButton>
-      </Box>
-      <Box variant="layout.verticalAlign" sx={{ gap: '12px' }}>
-        <Text sx={{ fontSize: 1 }}>Current {symbol} balance:</Text>
-        <IconButton
-          sx={{
-            width: 'max-content',
-            px: '12px',
-            cursor: 'pointer',
-            border: '1px solid',
-            borderColor: 'border',
-            borderRadius: '6px',
-            ':hover': { backgroundColor: 'border' },
-          }}
-        >
-          <Box variant="layout.verticalAlign" sx={{ gap: '6px' }}>
-            <WalletOutlineIcon fontSize={17} />
-            {/* TODO: replace with actual balance */}
-            <Text sx={{ fontSize: 1, fontWeight: 'strong', mt: '1px' }}>
-              0.00
-            </Text>
-            <ChevronRight size={16} strokeWidth={1.2} />
-          </Box>
-        </IconButton>
+          <Text sx={{ visibility: 'hidden' }}>{amount}</Text>
+          <Text sx={{ userSelect: 'none' }} ml="2" variant="legend">
+            RSR
+          </Text>
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+type ZapInputContainerProps = {
+  symbol: string
+  amount: string
+  setAmount: (amount: string) => void
+}
+
+const ZapInputContainer: FC<ZapInputContainerProps> = ({
+  symbol,
+  amount,
+  setAmount,
+}) => {
+  return (
+    <Box
+      variant="layout.centered"
+      sx={{
+        overflow: 'hidden',
+        backgroundColor: 'lightGrey',
+        borderRadius: '8px',
+        gap: '8px',
+        alignItems: 'start',
+      }}
+      p={3}
+    >
+      <Text>You use:</Text>
+      <ZapInput symbol={symbol} amount={amount} setAmount={setAmount} />
+
+      <Box variant="layout.verticalAlign">
+        {/* <StakeUsdAmount /> */}
+        {/* <StakeBalance /> */}
       </Box>
     </Box>
   )
 }
 
-const IssuanceInput: FC = () => {
-  return <Box>Issuance Input</Box>
+type ZapOutputContainerProps = {
+  inputSymbol: string
+  outputSymbol: string
+  amount: string
 }
 
-const IssuanceOperationDetails: FC = () => {
-  return <Box>Issuance Operation Details</Box>
+const ZapOutputContainer: FC<ZapOutputContainerProps> = ({
+  inputSymbol,
+  outputSymbol,
+  amount,
+}) => {
+  const rate = 1
+  const price = 1
+
+  return (
+    <Box
+      variant="layout.centered"
+      sx={{
+        border: '1px solid',
+        borderColor: 'border',
+        borderRadius: '8px',
+        overflow: 'hidden',
+        gap: '8px',
+        alignItems: 'start',
+      }}
+      p={3}
+    >
+      <Text sx={{ display: 'block' }}>You receive:</Text>
+      <Box
+        variant="layout.verticalAlign"
+        sx={{ fontSize: 4, fontWeight: 700, overflow: 'hidden' }}
+      >
+        <Text>{amount ? formatCurrency(Number(amount) * rate) : '0'}</Text>
+        <Text variant="legend" ml="2">
+          {outputSymbol}
+        </Text>
+      </Box>
+      <Box variant="layout.verticalAlign">
+        <Text
+          variant="legend"
+          sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
+        >
+          ${formatCurrency(price * Number(amount), 2)}
+        </Text>
+      </Box>
+    </Box>
+  )
 }
 
-const IssuanceSubmit: FC = () => {
-  return <Box>Issuance Submit</Box>
+const ZapOperationDetails: FC = () => {
+  return <Box></Box>
 }
 
 type RTokenIssuanceProps = {
@@ -87,6 +174,8 @@ type RTokenIssuanceProps = {
 }
 
 const RTokenIssuance: FC<RTokenIssuanceProps> = ({ rToken }) => {
+  const [amount, setAmount] = useState<string>('')
+
   return (
     <Box
       sx={{
@@ -95,16 +184,48 @@ const RTokenIssuance: FC<RTokenIssuanceProps> = ({ rToken }) => {
         alignSelf: 'stretch',
         borderRadius: '14px',
         bg: 'background',
+        border: '1px solid',
+        borderColor: 'border',
       }}
     >
       <Box p="24px">
-        <IssuanceTabs symbol={rToken.symbol} />
+        <ZapTabs symbol={rToken.symbol} />
       </Box>
       <Divider m={0} />
-      <Box p="24px">
-        <IssuanceInput />
-        <IssuanceOperationDetails />
-        <IssuanceSubmit />
+      <Box
+        p="24px"
+        sx={{ display: 'flex', flexDirection: 'column', gap: '24px' }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <ZapInputContainer
+            symbol={'USDC'}
+            amount={amount}
+            setAmount={setAmount}
+          />
+          <Box variant="layout.verticalAlign" sx={{ gap: '12px', px: 3 }}>
+            <Divider sx={{ flexGrow: 1 }} />
+            <Box
+              p="1"
+              pb="0"
+              sx={{
+                border: '1px solid',
+                borderColor: 'border',
+                borderRadius: '6px',
+                backgroundColor: 'lightGrey',
+              }}
+            >
+              <ArrowDown size={24} strokeWidth={1.2} color="#666666" />
+            </Box>
+            <Divider sx={{ flexGrow: 1 }} />
+          </Box>
+          <ZapOutputContainer
+            inputSymbol={'USDC'}
+            outputSymbol={rToken.symbol}
+            amount={amount}
+          />
+        </Box>
+        <ZapOperationDetails />
+        <Button sx={{ width: '100%' }}>Zap mint</Button>
       </Box>
     </Box>
   )
