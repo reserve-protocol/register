8bb7a3cbcfd69a18a982b2a765ede8c5491142f9
Luis Camargo <luiska1993@gmail.com>
2024-03-27 15:44:03 -0600

    feat: new base collaterals

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -109,7 +109,7 @@ export const ROUTES = Object.freeze({
   STAKING: 'staking',
   ISSUANCE: 'issuance',
   AUCTIONS: 'auctions',
-  DEPLOY: 'deploy',
+  DEPLOY: '/deploy',
   SETTINGS: 'settings',
   GOVERNANCE_SETUP: 'governance/setup',
   GOVERNANCE_INFO: 'governance-info',
@@ -248,4 +248,5 @@ export const collateralDisplay: Record<string, string> = {
   saethusdc: 'AAVE USDC V3',
   stkcvxpyusdusdc: 'Convex PYUSD/USDC',
   saethpyusd: 'AAVE PYUSD V3',
+  sabasusdc: 'AAVE USDC V3',
 }
