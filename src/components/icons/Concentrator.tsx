6efedb343d02aea4399b2204807d65875c84676d
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-03-28 12:55:31 -0300

    chore: change project icon

--- src/components/icons/Concentrator.tsx
+++ src/components/icons/Concentrator.tsx
@@ -1,6 +1,6 @@
 import { Image, ImageProps } from 'theme-ui'
 
 const Concentrator = (props: ImageProps) => {
-  return <Image width={16} src="/imgs/concentrator.jpg" {...props} />
+  return <Image width={16} src="/imgs/concentrator.png" {...props} />
 }
 export default Concentrator
