ad9a221f48adc37744c8f4cd64bd7977fc903973
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-04-01 12:21:56 -0300

    fix: tvl

--- src/views/home/hooks/useProtocolMetrics.ts
+++ src/views/home/hooks/useProtocolMetrics.ts
@@ -28,6 +28,13 @@ interface ProtocolMetricsResponse {
   }
 }
 
+type TokenMetrics = {
+  tokens: {
+    lastPriceUSD: string
+    totalSupply: bigint
+  }[]
+}
+
 const protocolMetricsQuery = gql`
   query GetProtocolMetrics($id: String!) {
     tokens(where: { id: "0x196f4727526ea7fb1e17b2071b3d8eaa38486988" }) {
@@ -50,6 +57,15 @@ const protocolMetricsQuery = gql`
   }
 `
 
+const tokensQuery = gql`
+  query GetTokens {
+    tokens(where: { totalSupply_gt: 0, lastPriceUSD_gt: 0 }, first: 1000) {
+      totalSupply
+      lastPriceUSD
+    }
+  }
+`
+
 const useProtocolMetrics = () => {
   const [stats, setStats] = useAtom(homeMetricsAtom)
   const rsrPrice = useAtomValue(rsrPriceAtom)
@@ -61,8 +77,14 @@ const useProtocolMetrics = () => {
     { keepPreviousData: true }
   )
 
+  const { data: tokens, isLoading: isLoadingTokens } = useMultichainQuery(
+    tokensQuery,
+    {},
+    { keepPreviousData: true }
+  )
+
   useEffect(() => {
-    if (data) {
+    if (data && tokens) {
       let volume = 0
       let marketCap = 0
       let stakeRevenue = 0
@@ -70,11 +92,16 @@ const useProtocolMetrics = () => {
 
       for (const chain of supportedChainList) {
         const metrics = data[chain] as ProtocolMetricsResponse
+        const _tokens = tokens[chain] as TokenMetrics
+        const tokensMarketCap = _tokens.tokens.reduce(
+          (acc, token) =>
+            acc + +formatEther(token.totalSupply) * +token.lastPriceUSD,
+          0
+        )
 
         if (metrics.protocol) {
           volume += +metrics.protocol.cumulativeVolumeUSD
-          // TODO: Wrong marketcap returned from mainnet, investigating
-          marketCap += chain === 1 ? 46301866 : +metrics.protocol.totalRTokenUSD
+          marketCap += tokensMarketCap
           stakeRevenue += +metrics.protocol.cumulativeRSRRevenueUSD
         }
 
@@ -98,11 +125,11 @@ const useProtocolMetrics = () => {
       // Set atom for cache
       setStats({ volume, marketCap, stakeRevenue, tvl })
     }
-  }, [data, rsrPrice])
+  }, [data, rsrPrice, tokens])
 
   return useMemo(() => {
-    return { data: stats, isLoading }
-  }, [stats])
+    return { data: stats, isLoading: isLoading || isLoadingTokens }
+  }, [stats, isLoading, isLoadingTokens])
 }
 
 export default useProtocolMetrics
