bd4cd20e52d09fabd0c337188f229165530a57c3
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-04-12 17:05:03 -0300

    chore: improve global throttle info

--- src/components/help/index.tsx
+++ src/components/help/index.tsx
@@ -1,17 +1,27 @@
+import { Placement } from '@popperjs/core'
 import HelpIcon from 'components/icons/HelpIcon'
 import { MouseoverTooltip } from 'components/tooltip'
+import { ReactNode } from 'react'
 import { Box, BoxProps } from 'theme-ui'
 
 interface Props extends BoxProps {
-  content: any
+  content: ReactNode
   size?: number
   color?: string
+  placement?: Placement
 }
 
-const Help = ({ content, color, size = 16, sx = {}, ...props }: Props) => {
+const Help = ({
+  content,
+  color,
+  size = 16,
+  sx = {},
+  placement,
+  ...props
+}: Props) => {
   const defaultColor = 'secondaryText'
   return (
-    <MouseoverTooltip text={content}>
+    <MouseoverTooltip text={content} placement={placement}>
       <Box
         variant="layout.verticalAlign"
         sx={{ cursor: 'pointer', ...sx }}
