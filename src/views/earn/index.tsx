f4f025c0f41c5af54c9cc96a083fd1afba0587ae
Luis Camargo <luiska1993@gmail.com>
2024-04-12 17:00:31 -0700

    chore: add heros

--- src/views/earn/index.tsx
+++ src/views/earn/index.tsx
@@ -1,8 +1,68 @@
 import { useEffect } from 'react'
-import { Box } from 'theme-ui'
+import { Box, Flex, Text } from 'theme-ui'
 import Earn from './components/Earn'
 import RegisterAbout from 'views/home/components/RegisterAbout'
 import mixpanel from 'mixpanel-browser'
+import { Trans } from '@lingui/macro'
+
+const HeroBackground = () => {
+  const url = '/imgs/bg-earn.png'
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '484px',
+        top: 0,
+        zIndex: -1,
+        position: 'absolute',
+        background: `url(${url}) no-repeat`,
+        backgroundSize: 'cover',
+        borderBottom: '3px solid',
+        borderColor: 'borderFocused',
+      }}
+    />
+  )
+}
+
+const Hero = () => (
+  <Box sx={{ position: 'relative' }}>
+    <Flex
+      mx="auto"
+      sx={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        position: 'relative',
+        maxWidth: '95em',
+      }}
+      pt={[5, 4]}
+      mt={[0, 5]}
+      pb={0}
+      px={[2, 3]}
+    >
+      <Box sx={{ maxWidth: 900, textAlign: 'center' }} mt={[2, 4]}>
+        <Text
+          variant="title"
+          sx={{
+            fontSize: [5, 7],
+            fontWeight: 'bold',
+            color: 'accentInverted',
+            lineHeight: ['36px', '62px'],
+          }}
+        >
+          <Trans>
+            Provide liquidity across DeFi & Earn more with your RTokens
+          </Trans>
+        </Text>
+        <Text as="p" px={[2, 0]} sx={{ fontSize: [2, 3] }} mt={[3, 4]}>
+          <Trans>
+            DeFi yield opportunities for RTokens in Convex, Curve, Yearn & Beefy
+          </Trans>
+        </Text>
+      </Box>
+    </Flex>
+  </Box>
+)
 
 const EarnWrapper = () => {
   useEffect(() => {
@@ -11,10 +71,11 @@ const EarnWrapper = () => {
 
   return (
     <>
-      <Box
-        variant="layout.wrapper"
-        sx={{ borderTop: '1px solid', borderColor: 'border' }}
-      >
+      <Box sx={{ position: 'relative' }}>
+        <HeroBackground />
+        <Hero />
+      </Box>
+      <Box variant="layout.wrapper">
         <Earn />
       </Box>
       <RegisterAbout />
