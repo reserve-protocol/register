90cddccca32973f6160932dbca90dd88ac105d75
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-05-14 14:39:18 -0300

    fix: tvl computation

--- src/views/home/hooks/useProtocolMetrics.ts
+++ src/views/home/hooks/useProtocolMetrics.ts
@@ -4,19 +4,9 @@ import { useAtom, useAtomValue } from 'jotai'
 import { useEffect, useMemo } from 'react'
 import { rsrPriceAtom } from 'state/atoms'
 import { PROTOCOL_SLUG, supportedChainList } from 'utils/constants'
-import { formatEther } from 'viem'
 import { homeMetricsAtom } from '../atoms'
 
-type TokenMetrics = {
-  id: string
-  lastPriceUSD: string
-  totalSupply: bigint
-  cumulativeVolume: bigint
-  transferCount: string
-}
-
 type ProtocolMetricsResponse = {
-  tokens: TokenMetrics[]
   protocol: {
     totalValueLockedUSD: string
     totalRTokenUSD: string
@@ -32,13 +22,6 @@ type ProtocolMetricsResponse = {
 
 const protocolMetricsQuery = gql`
   query GetProtocolMetrics($id: String!) {
-    tokens(where: { totalSupply_gt: 0, lastPriceUSD_gt: 0 }, first: 1000) {
-      id
-      lastPriceUSD
-      totalSupply
-      cumulativeVolume
-      transferCount
-    }
     protocol(id: $id) {
       totalValueLockedUSD
       totalRTokenUSD
@@ -74,25 +57,13 @@ const useProtocolMetrics = () => {
       for (const chain of supportedChainList) {
         const metrics = data[chain] as ProtocolMetricsResponse
 
-        if (metrics?.protocol && metrics?.tokens) {
-          const tokens = metrics.tokens as TokenMetrics[]
-          const tokensMarketCap = tokens.reduce(
-            (acc, token) =>
-              acc + +formatEther(token.totalSupply) * +token.lastPriceUSD,
-            0
-          )
-
-          if (metrics.protocol) {
-            volume += +metrics.protocol.cumulativeVolumeUSD
-            marketCap += tokensMarketCap
-            stakeRevenue += +metrics.protocol.cumulativeRSRRevenueUSD
-          }
-
-          tvl += +formatEther(metrics.protocol.rsrStaked as any) * rsrPrice
+        if (metrics?.protocol) {
+          volume += +metrics.protocol.cumulativeVolumeUSD
+          marketCap += +metrics.protocol.totalRTokenUSD
+          stakeRevenue += +metrics.protocol.cumulativeRSRRevenueUSD
+          tvl += +metrics.protocol.totalValueLockedUSD
         }
       }
-
-      tvl += marketCap
 
       // Set atom for cache
       setStats({ volume, marketCap, stakeRevenue, tvl })
