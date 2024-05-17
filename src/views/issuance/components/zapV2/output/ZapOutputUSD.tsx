ef54298e2008a6049383ac3aa82157a8d28f743a
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-05-17 13:10:22 -0300

    fix: zap price impact

--- src/views/issuance/components/zapV2/output/ZapOutputUSD.tsx
+++ src/views/issuance/components/zapV2/output/ZapOutputUSD.tsx
@@ -2,6 +2,7 @@ import Skeleton from 'react-loading-skeleton'
 import { Box, Text } from 'theme-ui'
 import { formatCurrency } from 'utils'
 import { useZap } from '../context/ZapContext'
+import Help from 'components/help'
 
 const ZapOutputUSD = () => {
   const { tokenOut, amountOut, zapDustUSD, loadingZap } = useZap()
@@ -11,7 +12,7 @@ const ZapOutputUSD = () => {
   }
 
   return (
-    <Box>
+    <Box variant="layout.verticalAlign" sx={{ gap: 1 }}>
       <Text
         variant="legend"
         sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
@@ -19,14 +20,20 @@ const ZapOutputUSD = () => {
         ${formatCurrency((tokenOut?.price || 0) * Number(amountOut), 2)}
       </Text>
       {zapDustUSD !== undefined && zapDustUSD !== 0 && (
-        <Text>
-          {' '}
-          +{' '}
-          <Text variant="legend" sx={{ fontWeight: 'strong' }}>
-            ${formatCurrency(+zapDustUSD, 2)}
-          </Text>{' '}
-          in dust
-        </Text>
+        <>
+          <Text>
+            {' '}
+            +{' '}
+            <Text variant="legend" sx={{ fontWeight: 'strong' }}>
+              ${formatCurrency(+zapDustUSD, 2)}
+            </Text>{' '}
+            in dust
+          </Text>
+          <Help
+            mt="2px"
+            content={`Dust is the leftover amount of tokens that cannot be exchanged or included in the RToken mint, due to the zapper route. It will be sent back to your wallet.`}
+          />
+        </>
       )}
     </Box>
   )
