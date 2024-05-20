79bc6b5a27c09fe3de153180ae2c245c5396991a
Luis Camargo <luiska1993@gmail.com>
2024-05-20 16:48:20 -0600

    feat: add filters

--- src/views/explorer/components/governance/index.tsx
+++ src/views/explorer/components/governance/index.tsx
@@ -1,61 +1,17 @@
 import { Trans, t } from '@lingui/macro'
 import { createColumnHelper } from '@tanstack/react-table'
-import ChainLogo from 'components/icons/ChainLogo'
-import DebankIcon from 'components/icons/DebankIcon'
-import TransactionsIcon from 'components/icons/TransactionsIcon'
+import GovernanceIcon from 'components/icons/GovernanceIcon'
 import { Table } from 'components/table'
 import TokenItem from 'components/token-item'
 import dayjs from 'dayjs'
-import { gql } from 'graphql-request'
-import { useMultichainQuery } from 'hooks/useQuery'
 import useRTokenLogo from 'hooks/useRTokenLogo'
-import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
-import { publicClient } from 'state/chain'
 import { Badge, Box, Link, Text } from 'theme-ui'
 import { StringMap } from 'types'
-import {
-  formatCurrency,
-  formatUsdCurrencyCell,
-  getProposalTitle,
-  shortenAddress,
-  shortenString,
-} from 'utils'
-import { atomWithLoadable } from 'utils/atoms/utils'
-import { ChainId } from 'utils/chains'
-import {
-  PROPOSAL_STATES,
-  formatConstant,
-  supportedChainList,
-} from 'utils/constants'
-import { ExplorerDataType, getExplorerLink } from 'utils/getExplorerLink'
-import { formatEther, getAddress, isAddress } from 'viem'
-import { getProposalStatus } from 'views/governance/views/proposal-detail/atom'
-
-const explorerProposalsQuery = gql`
-  query getAllProposals {
-    proposals(orderBy: creationTime, orderDirection: desc) {
-      id
-      description
-      creationTime
-      state
-      governance
-      forWeightedVotes
-      againstWeightedVotes
-      quorumVotes
-      startBlock
-      endBlock
-      governance {
-        rToken {
-          id
-          token {
-            symbol
-          }
-        }
-      }
-    }
-  }
-`
+import { getProposalTitle, getTokenRoute } from 'utils'
+import { PROPOSAL_STATES, ROUTES, formatConstant } from 'utils/constants'
+import useProposalsData, { ProposalRecord } from './useProposalsData'
+import Filters from './Filters'
 
 const BADGE_VARIANT: StringMap = {
   [PROPOSAL_STATES.DEFEATED]: 'danger',
@@ -65,69 +21,8 @@ const BADGE_VARIANT: StringMap = {
   [PROPOSAL_STATES.CANCELED]: 'danger',
 }
 
-export interface ProposalRecord {
-  id: string
-  description: string
-  creationTime: string
-  state: string
-  governance: string
-  forWeightedVotes: string
-  againstWeightedVotes: string
-  quorumVotes: string
-  startBlock: string
-  endBlock: string
-  status: string
-  rTokenAddress: string
-  rTokenSymbol: string
-  chain: number
-}
-
-const chainBlocksAtom = atomWithLoadable(async () => {
-  const result = await Promise.all(
-    supportedChainList.map((chain) =>
-      publicClient({ chainId: chain }).getBlockNumber()
-    )
-  )
-
-  return supportedChainList.reduce((acc, chain, index) => {
-    acc[chain] = Number(result[index])
-    return acc
-  }, {} as Record<number, number>)
-})
-
-const useProposals = () => {
-  const blocks = useAtomValue(chainBlocksAtom)
-  const { data, error } = useMultichainQuery(explorerProposalsQuery)
-
-  return useMemo(() => {
-    if (!data) return []
-
-    const proposals: ProposalRecord[] = []
-
-    for (const chain of supportedChainList) {
-      if (data[chain]) {
-        proposals.push(
-          ...data[chain].proposals.map((entry: any) => {
-            const status = getProposalStatus(entry, blocks?.[chain] || 0)
-
-            return {
-              ...entry,
-              status,
-              chain,
-              rTokenAddress: getAddress(entry.governance.rToken.id),
-              rTokenSymbol: entry.governance.rToken.token.symbol,
-            }
-          })
-        )
-      }
-    }
-
-    return proposals
-  }, [data, blocks])
-}
-
 const ExploreGovernance = () => {
-  const data = useProposals()
+  const data = useProposalsData()
   const columnHelper = createColumnHelper<ProposalRecord>()
   const columns = useMemo(
     () => [
@@ -145,9 +40,19 @@ const ExploreGovernance = () => {
       columnHelper.accessor('description', {
         header: t`Description`,
         cell: (data) => (
-          <Text sx={{ textTransform: 'capitalize' }}>
-            {getProposalTitle(data.getValue())}
-          </Text>
+          <Link
+            href={getTokenRoute(
+              data.row.original.rTokenAddress,
+              data.row.original.chain,
+              `${ROUTES.GOVERNANCE_PROPOSAL}/${data.row.original.id}`
+            )}
+            target="_blank"
+            sx={{ textDecoration: 'underline' }}
+          >
+            <Text sx={{ textTransform: 'capitalize' }}>
+              {getProposalTitle(data.getValue())}
+            </Text>
+          </Link>
         ),
       }),
       columnHelper.accessor('creationTime', {
@@ -168,77 +73,6 @@ const ExploreGovernance = () => {
           </Badge>
         ),
       }),
-      // columnHelper.accessor('amount', {
-      //   header: t`Amount`,
-      //   cell: (data) => {
-      //     const parsed = formatEther(data.getValue())
-      //     let symbol = data.row.original.token.symbol
-      //     if (
-      //       data.row.original.type === 'STAKE' ||
-      //       data.row.original.type === 'UNSTAKE' ||
-      //       data.row.original.type === 'WITHDRAW' ||
-      //       data.row.original.type === 'DEPOSIT' ||
-      //       data.row.original.type === 'WITHDRAWAL'
-      //     ) {
-      //       symbol = 'RSR'
-      //     }
-      //     return `${formatCurrency(+parsed)} ${symbol}`
-      //   },
-      // }),
-      // columnHelper.accessor('amountUSD', {
-      //   header: t`USD Value`,
-      //   cell: (data) => {
-      //     if (isNaN(+data.getValue())) {
-      //       return `$${data.getValue()}`
-      //     }
-      //     return formatUsdCurrencyCell(data as any)
-      //   },
-      // }),
-      // columnHelper.accessor('timestamp', {
-      //   header: t`Time`,
-      //   cell: (data) => dayjs.unix(data.getValue()).format('YYYY-M-D HH:mm'),
-      // }),
-      // columnHelper.accessor('from.id', {
-      //   header: t`From`,
-      //   cell: (data) => {
-      //     const address =
-      //       data.row.original.type === 'MINT' ||
-      //       data.row.original.type === 'ISSUE'
-      //         ? data.row.original.to.id
-      //         : data.getValue()
-      //     return (
-      //       <Box variant="layout.verticalAlign">
-      //         <Link
-      //           href={`https://debank.com/profile/${address}`}
-      //           target="_blank"
-      //           mr="2"
-      //         >
-      //           {shortenAddress(address)}
-      //         </Link>
-      //         <DebankIcon />
-      //       </Box>
-      //     )
-      //   },
-      // }),
-      // columnHelper.accessor('chain', {
-      //   header: t`Platform`,
-      //   cell: (data) => {
-      //     return (
-      //       <Link
-      //         href={getExplorerLink(
-      //           data.row.original.hash,
-      //           data.row.original.chain,
-      //           ExplorerDataType.TRANSACTION
-      //         )}
-      //         target="_blank"
-      //         sx={{ display: 'flex', alignItems: 'center' }}
-      //       >
-      //         <ChainLogo style={{ marginRight: 10 }} chain={data.getValue()} />
-      //         {shortenString(data.row.original.hash)}
-      //       </Link>
-      //     )
-      //   },
-      // }),
     ],
     []
   )
@@ -246,10 +80,11 @@ const ExploreGovernance = () => {
   return (
     <Box mt={5} mx={[1, 4]}>
       <Box variant="layout.verticalAlign" mb={5}>
-        <TransactionsIcon fontSize={32} />
+        <GovernanceIcon fontSize={32} />
         <Text ml="2" as="h2" variant="title" sx={{ fontSize: 4 }}>
           <Trans>Proposals</Trans>
         </Text>
+        <Filters />
       </Box>
       <Table
         sorting
@@ -258,7 +93,6 @@ const ExploreGovernance = () => {
         pagination={{ pageSize: 10 }}
         columns={columns}
         sx={{ borderRadius: '0 0 20px 20px' }}
-        isLoading={!data.length}
         compact
       />
     </Box>
