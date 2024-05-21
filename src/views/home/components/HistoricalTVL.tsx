3365f10f62f5de8663c5388c94eef1f929189f01
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-05-21 12:00:36 -0300

    chore: change tvl chart tooltip

--- src/views/home/components/HistoricalTVL.tsx
+++ src/views/home/components/HistoricalTVL.tsx
@@ -1,3 +1,5 @@
+import ChainLogo from 'components/icons/ChainLogo'
+import SmallRootIcon from 'components/icons/SmallRootIcon'
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts'
 import { colors } from 'theme'
 import { Box, Card, Text } from 'theme-ui'
@@ -37,21 +39,69 @@ function CustomTooltip({ payload, label, active }: any) {
         display: 'flex',
         flexDirection: 'column',
         border: '2px solid',
-        borderColor: 'border',
+        borderColor: 'reserveBackground',
         background: 'cardAlternative',
+        gap: 3,
+        p: 0,
       }}
     >
-      <Text>{new Date(label).toDateString()}</Text>
-      {(payload as any[]).map(
-        (item: { name: string; value: number }, index) => (
-          <Box key={`${item.name}${item.value}${index}`}>
-            <Text>
-              {capitalize(item.name)}: ${formatCurrency(item.value, 0)}
-            </Text>
-          </Box>
-        )
-      )}
-      <Text>Total: ${formatCurrency(total, 0)}</Text>
+      <Box
+        variant="layout.verticalAlign"
+        sx={{
+          gap: 3,
+          justifyContent: 'space-between',
+          borderBottom: '1px solid',
+          borderColor: 'border',
+        }}
+        p={3}
+      >
+        <Text sx={{ fontSize: 1 }}>{new Date(label).toDateString()}</Text>
+        <Text color="secondaryText" sx={{ fontSize: 1 }}>
+          (TVL per network)
+        </Text>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+        }}
+        px={3}
+      >
+        {(payload as any[]).map(
+          (item: { name: string; value: number }, index) => (
+            <Box
+              key={`${item.name}${item.value}${index}`}
+              variant="layout.verticalAlign"
+              sx={{ gap: 2, justifyContent: 'space-between' }}
+            >
+              <Box variant="layout.verticalAlign" sx={{ gap: '6px' }}>
+                <ChainLogo chain={NETWORKS[item.name]} />
+                <Text>{capitalize(item.name)}:</Text>
+              </Box>
+              <Text variant="bold">${formatCurrency(item.value, 0)}</Text>
+            </Box>
+          )
+        )}
+      </Box>
+      <Box
+        variant="layout.verticalAlign"
+        sx={{
+          gap: 2,
+          justifyContent: 'space-between',
+          borderTop: '1px solid',
+          borderColor: 'border',
+        }}
+        p={3}
+      >
+        <Box variant="layout.verticalAlign" sx={{ gap: '6px' }}>
+          <SmallRootIcon />
+          <Text>Total TVL:</Text>
+        </Box>
+        <Text color="accentInverted" variant="bold">
+          ${formatCurrency(total, 0)}
+        </Text>
+      </Box>
     </Card>
   )
 }
