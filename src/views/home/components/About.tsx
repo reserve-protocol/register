439986fff89acd825ac83e61aa78051929528144
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-05-22 15:41:28 -0300

    chore: minor adjustment on mobile

--- src/views/home/components/About.tsx
+++ src/views/home/components/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
           objectFit: ['none', 'cover'],
           objectPosition: ['left 60%', 'left top'],
           width: '100%',
-          height: '160px',
+          height: [200, 160],
           maxHeight: ['200px', '100%'],
           minHeight: ['50px', '100%'],
           borderRadius: [0, '14px'],
--- src/views/home/components/Hero.tsx
+++ src/views/home/components/Hero.tsx
@@ -276,7 +276,7 @@ const Hero = () => (
       position: 'relative',
       height: '100%',
       overflow: 'hidden',
-      minHeight: 480,
+      minHeight: [560, 480],
     }}
   >
     <Flex
