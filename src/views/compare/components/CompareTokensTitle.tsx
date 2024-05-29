69f592af56b67964d1715324b9c4932d0be50d6d
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-05-29 15:36:23 -0300

    chore: change compare page background image

--- src/views/compare/components/CompareTokensTitle.tsx
+++ src/views/compare/components/CompareTokensTitle.tsx
@@ -1,23 +1,28 @@
 import { Trans } from '@lingui/macro'
 import BasketCubeIcon from 'components/icons/BasketCubeIcon'
-import { Box, BoxProps, Text } from 'theme-ui'
+import { Box, BoxProps, Text, useColorMode } from 'theme-ui'
 import CompareFilters from './CompareFilters'
 
-const CompareBg = (props: BoxProps) => (
-  <Box
-    sx={{
-      display: ['none', 'none', 'block'],
-      width: '400px',
-      height: '160px',
-      top: 0,
-      zIndex: -1,
-      position: 'absolute',
-      background: `url(/imgs/bg-compare.png) no-repeat`,
-      backgroundSize: 'cover',
-      ...props.sx,
-    }}
-  />
-)
+const CompareBg = (props: BoxProps) => {
+  const [colorMode] = useColorMode()
+
+  return (
+    <Box
+      sx={{
+        display: ['none', 'none', 'block'],
+        width: '400px',
+        height: '158px',
+        top: 0,
+        zIndex: -1,
+        position: 'absolute',
+        background: `url(/imgs/bg-compare.png) no-repeat`,
+        backgroundSize: 'contain',
+        opacity: colorMode === 'dark' ? 0.25 : 1,
+        ...props.sx,
+      }}
+    />
+  )
+}
 
 const CompareTokensTitle = () => {
   return (
