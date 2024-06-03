e50bec1c99076729d4ad5c4085de2d9d2588c1ab
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-06-03 16:46:17 -0300

    chore: remove import

--- src/views/governance/components/ProposalDetailPreview.tsx
+++ src/views/governance/components/ProposalDetailPreview.tsx
@@ -4,7 +4,6 @@ import { Box, BoxProps, Card, Spinner, Text } from 'theme-ui'
 import { Hex, decodeFunctionData, getAbiItem, getAddress } from 'viem'
 import { ContractProposal, InterfaceMap, interfaceMapAtom } from '../atoms'
 import ContractProposalDetail from '../views/proposal-detail/components/ContractProposalDetails'
-import { add } from '@dnd-kit/utilities'
 
 interface Props extends BoxProps {
   addresses: string[]
