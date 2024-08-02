578889c8598341c7280d15f91203798c6bda2649
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-08-02 14:14:53 -0300

    feat: backing buffer ui

--- src/views/overview/components/backing/BackingBuffer.tsx
+++ src/views/overview/components/backing/BackingBuffer.tsx
@@ -1,239 +1,123 @@
 import Help from 'components/help'
+import BackingBufferIcon from 'components/icons/BackingBufferIcon'
+import ProgressBar from 'components/progress-bar'
+import useRToken from 'hooks/useRToken'
 import { useAtomValue } from 'jotai'
-import { ReactNode } from 'react'
-import { Database } from 'react-feather'
+import { useMemo } from 'react'
 import Skeleton from 'react-loading-skeleton'
-import { rTokenBackingDistributionAtom } from 'state/atoms'
+import {
+  rTokenBackingDistributionAtom,
+  rTokenPriceAtom,
+  rTokenStateAtom,
+} from 'state/atoms'
 import { Box, BoxProps, Text } from 'theme-ui'
-
-interface ProgressBarProps {
-  percentage: number
-  foregroundText?: ReactNode
-  backgroundText?: ReactNode
-  height?: number
-  width?: number | string
-}
-
-const ProgressBar: React.FC<ProgressBarProps> = ({
-  percentage,
-  foregroundText,
-  backgroundText,
-  height = 40,
-  width = '100%',
-}) => {
-  const shouldPercentageBeOnForeground = percentage <= 8
-  const shouldTextBeOnForeground = percentage >= 40
-  const hideBackgroundText = percentage >= 70
-
-  return (
-    <Box
-      sx={{
-        position: 'relative',
-        height,
-        width,
-        borderRadius: '8px',
-        overflow: 'hidden',
-        border: '1px solid',
-        borderColor: 'black',
-      }}
-    >
-      <Box
-        sx={{
-          height: '100%',
-          width: `${percentage}%`,
-          backgroundColor: 'black',
-          transition: 'width 0.5s ease-in-out',
-          position: 'relative',
-        }}
-      >
-        {!shouldTextBeOnForeground && !shouldPercentageBeOnForeground && (
-          <Text
-            sx={{
-              display: ['none', 'none', 'block'],
-              position: 'absolute',
-              top: '50%',
-              right: 3,
-              transform: 'translateY(-50%)',
-              color: 'white',
-              fontSize: 1,
-              fontWeight: 'bold',
-            }}
-          >
-            {`${percentage}%`}
-          </Text>
-        )}
-      </Box>
-      {foregroundText && (
-        <Text
-          sx={{
-            display: ['none', 'none', 'block'],
-            position: 'absolute',
-            top: '50%',
-            left: shouldTextBeOnForeground
-              ? `${percentage}%`
-              : `${percentage}%`,
-            transform: shouldTextBeOnForeground
-              ? 'translate(-100%, -50%)'
-              : 'translateY(-50%)',
-            color: shouldTextBeOnForeground ? 'white' : 'text',
-            fontSize: 1,
-            paddingLeft: shouldTextBeOnForeground ? 2 : 2,
-            paddingRight: shouldTextBeOnForeground ? 3 : 2,
-            whiteSpace: 'nowrap',
-          }}
-        >
-          {foregroundText}
-          {(shouldTextBeOnForeground || shouldPercentageBeOnForeground) && (
-            <>
-              <Text sx={{ mx: 2 }}>|</Text>
-              <Text
-                sx={{
-                  color: shouldPercentageBeOnForeground ? 'black' : 'white',
-                  fontSize: 1,
-                  fontWeight: 'bold',
-                }}
-              >
-                {`${percentage}%`}
-              </Text>
-            </>
-          )}
-        </Text>
-      )}
-      {backgroundText && !hideBackgroundText && (
-        <Text
-          sx={{
-            display: ['none', 'none', 'block'],
-            position: 'absolute',
-            top: '50%',
-            right: 3,
-            transform: 'translateY(-50%)',
-            color: 'black',
-            fontSize: 1,
-          }}
-        >
-          {backgroundText}
-        </Text>
-      )}
-      <Text
-        sx={{
-          display: ['block', 'block', 'none'],
-          position: 'absolute',
-          top: '50%',
-          right: 3,
-          transform: 'translateY(-50%)',
-          fontSize: 1,
-          whiteSpace: 'nowrap',
-          mixBlendMode: 'difference', // here the text could have different background colors (white or black)
-        }}
-      >
-        {foregroundText}
-        <>
-          <Text sx={{ mx: 2 }}>|</Text>
-          <Text
-            sx={{
-              fontSize: 1,
-              fontWeight: 'bold',
-            }}
-          >
-            {`${percentage}%`}
-          </Text>
-        </>
-      </Text>
-    </Box>
-  )
-}
+import { formatCurrency, formatPercentage } from 'utils'
 
 const BuckingBuffer = ({ ...props }: BoxProps) => {
+  const rToken = useRToken()
   const backing = useAtomValue(rTokenBackingDistributionAtom)
+  const rTokenState = useAtomValue(rTokenStateAtom)
+  const rTokenPrice = useAtomValue(rTokenPriceAtom)
 
-  if (!backing) {
-    return <Skeleton />
-  }
+  const [percentage, actual, required, percentageOfMCap] = useMemo(() => {
+    if (!backing) return [0, 0, 0]
+
+    const _actual = backing.backingBuffer.actual
+    const _required = backing.backingBuffer.required
+    const _percentage = (_actual / _required) * 100
+    const mCap = rTokenPrice * rTokenState.tokenSupply
+    const _percentageOfMCap = formatPercentage((_required / mCap) * 100)
+
+    return [
+      _percentage,
+      formatCurrency(_actual),
+      formatCurrency(_required),
+      _percentageOfMCap,
+    ]
+  }, [backing, rTokenPrice, rTokenState])
 
   return (
-    <Box px={4} {...props}>
-      <Box variant="layout.verticalAlign" sx={{ gap: 2 }}>
-        <Database strokeWidth={2} />
-        <Text variant="sectionTitle">Backing buffer</Text>
-        {/* <Text sx={{ fontSize: 3, opacity: 0.2, mt: 1, mx: 2 }}>|</Text> */}
-        <Help content="Collateral yield is distributed as revenue when the backing buffer is full." />
-      </Box>
-
-      <Box my={4}>
-        <ProgressBar
-          percentage={8.1}
-          foregroundText={
-            <Text>
-              <Text sx={{ display: ['none', 'inline', 'inline'] }}>
-                Current value in buffer:{' '}
-              </Text>
-              <Text sx={{ fontWeight: 'bold' }}>$663,769</Text>
+    <Box {...props}>
+      <Box
+        px={4}
+        sx={{
+          display: 'flex',
+          flexDirection: ['column', 'column', 'column', 'row', 'row'],
+          alignItems: [
+            'flex-start',
+            'flex-start',
+            'flex-start',
+            'center',
+            'center',
+          ],
+          gap: 2,
+          justifyContent: 'space-between',
+          '@media (min-width: 1150px) and (max-width: 1250px)': {
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+          },
+        }}
+      >
+        <Box
+          variant="layout.verticalAlign"
+          sx={{
+            gap: 2,
+            flexDirection: ['column', 'row'],
+            alignItems: ['flex-start', 'center'],
+          }}
+        >
+          <Box variant="layout.verticalAlign" sx={{ gap: 2 }}>
+            <BackingBufferIcon />
+            <Text variant="sectionTitle">Backing buffer</Text>
+          </Box>
+          <Box variant="layout.verticalAlign" sx={{ gap: 2 }}>
+            <Text
+              sx={{
+                fontSize: 3,
+                opacity: 0.2,
+                mx: 2,
+                display: ['none', 'inline'],
+              }}
+            >
+              |
             </Text>
-          }
-          backgroundText={
-            <Text>
-              100% at current mcap:{' '}
-              <Text sx={{ fontWeight: 'bold' }}>$3,318,848.05</Text>
+            <Text variant="contentTitle">
+              {rToken?.symbol || ''} buffer as % of mcap:
             </Text>
-          }
-        />
-      </Box>
-      <Box my={4}>
-        <ProgressBar
-          percentage={20}
-          foregroundText={
-            <Text>
-              <Text sx={{ display: ['none', 'inline', 'inline'] }}>
-                Current value in buffer:{' '}
-              </Text>
-              <Text sx={{ fontWeight: 'bold' }}>$663,769</Text>
-            </Text>
-          }
-          backgroundText={
-            <Text>
-              100% at current mcap:{' '}
-              <Text sx={{ fontWeight: 'bold' }}>$3,318,848.05</Text>
-            </Text>
-          }
-        />
-      </Box>
-
-      <ProgressBar
-        percentage={50}
-        foregroundText={
-          <Text>
-            <Text sx={{ display: ['none', 'inline', 'inline'] }}>
-              Current value in buffer:{' '}
-            </Text>
-            <Text sx={{ fontWeight: 'bold' }}>$1,659,424.025</Text>
+            <Text sx={{ fontSize: 1 }}>{percentageOfMCap}</Text>
+          </Box>
+        </Box>
+        <Box variant="layout.verticalAlign" sx={{ gap: 2 }}>
+          <Text variant="contentTitle">
+            Collateral yield is distributed as revenue when the backing buffer
+            is full
           </Text>
-        }
-        backgroundText={
-          <Text>
-            100% at current mcap:{' '}
-            <Text sx={{ fontWeight: 'bold' }}>$3,318,848.05</Text>
-          </Text>
-        }
-      />
+          <Help content="Collateral yield is distributed as revenue when the backing buffer is full." />
+        </Box>
+      </Box>
 
       <Box my={4}>
-        <ProgressBar
-          percentage={80}
-          foregroundText={
-            <Text>
-              <Text sx={{ display: ['none', 'inline', 'inline'] }}>
-                Current value in buffer:{' '}
+        {backing ? (
+          <ProgressBar
+            percentage={percentage}
+            foregroundText={
+              <Text>
+                <Text sx={{ display: ['none', 'inline', 'inline'] }}>
+                  Current value in buffer:{' '}
+                </Text>
+                <Text sx={{ fontWeight: 'bold' }}>${actual}</Text>
               </Text>
-              <Text sx={{ fontWeight: 'bold' }}>$2,655,078.44</Text>
-            </Text>
-          }
-          backgroundText={
-            <Text>
-              100% at current mcap:{' '}
-              <Text sx={{ fontWeight: 'bold' }}>$3,318,848.05</Text>
-            </Text>
-          }
-        />
+            }
+            backgroundText={
+              <Text>
+                100% at current mcap:{' '}
+                <Text sx={{ fontWeight: 'bold' }}>${required}</Text>
+              </Text>
+            }
+          />
+        ) : (
+          <Skeleton height={36} width="100%" />
+        )}
       </Box>
     </Box>
   )
