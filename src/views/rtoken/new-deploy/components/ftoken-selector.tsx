4fe7636434052196264c6dbad1ff15ebad049233
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-12-05 12:47:12 -0300

    chore: wip ftoken selector

--- src/views/rtoken/new-deploy/components/ftoken-selector.tsx
+++ src/views/rtoken/new-deploy/components/ftoken-selector.tsx
@@ -1,23 +1,35 @@
 import { Button } from '@/components/ui/button'
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
-import { PlusIcon } from 'lucide-react'
+import { PlusIcon, XIcon } from 'lucide-react'
+
+const OpenButton = () => (
+  <DrawerTrigger asChild>
+    <Button
+      variant="outline-primary"
+      className="flex gap-2 text-base pl-3 pr-4 py-5 rounded-xl"
+    >
+      <PlusIcon size={16} />
+      Add collateral
+    </Button>
+  </DrawerTrigger>
+)
+
+const CloseButton = () => (
+  <DrawerTrigger asChild className="w-max rounded-xl px-2 h-9">
+    <Button variant="outline">
+      <XIcon size={20} strokeWidth={1.5} />
+    </Button>
+  </DrawerTrigger>
+)
 
 const FTokenSelector = () => {
   return (
     <div className="flex items-center justify-center h-80 border-t border-b border-border mb-2">
       <Drawer direction="right">
-        <DrawerTrigger asChild>
-          <Button
-            variant="outline-primary"
-            className="flex gap-2 text-base pl-3 pr-4 py-5 rounded-xl"
-          >
-            <PlusIcon size={16} />
-            Add collateral
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className="fixed left-auto right-2 top-2 bottom-2 outline-none w-[310px] flex bg-transparent border-none mt-0">
+        <OpenButton />
+        <DrawerContent className="fixed left-auto right-2 top-2 bottom-2 outline-none w-[500px] flex bg-transparent border-none mt-0">
           <div className="bg-card h-full w-full grow p-5 flex flex-col rounded-[16px]">
-            FTokens
+            <CloseButton />
           </div>
         </DrawerContent>
       </Drawer>
