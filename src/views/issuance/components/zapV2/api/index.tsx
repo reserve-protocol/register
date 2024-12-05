d10c870b3628eed70c5b5ef5711dc7898d534859
Juan Pablo Rombola <juampi.rombola@gmail.com>
2024-12-05 11:54:17 -0300

    chore: zapper-api staging env

---
+++ src/views/issuance/components/zapV2/api/index.tsx
@@ -0,0 +1,64 @@
+import { Address } from 'viem'
+
+const BASE_ZAP_API_URL = 'https://zapper-api-stage.reserve.org'
+
+export type ZapPayload = {
+  chainId: number
+  tokenIn: Address
+  tokenOut: Address
+  amountIn: string
+  slippage: number
+  signer: Address
+  trade: boolean
+}
+
+export type ZapResult = {
+  tokenIn: string
+  amountIn: string
+  amountInValue: number | null
+
+  tokenOut: string
+  amountOut: string
+  amountOutValue: number | null
+
+  approvalAddress: Address
+  approvalNeeded: boolean
+  insufficientFunds: boolean
+
+  dust: {
+    token: string
+    amount: string
+  }[]
+  dustValue: number | null
+
+  gas: string | null
+  priceImpact: number // 0.0% => no impact | 10 => 10% impact
+  tx: {
+    data: string
+    to: Address
+    value: string
+  } | null
+}
+
+export type ZapResponse = {
+  status: 'success' | 'error'
+  result?: ZapResult
+  error?: string
+}
+
+export const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+const zapper = {
+  zap: ({
+    chainId,
+    tokenIn,
+    tokenOut,
+    amountIn,
+    slippage,
+    signer,
+    trade,
+  }: ZapPayload) =>
+    `${BASE_ZAP_API_URL}/?chainId=${chainId}&signer=${signer}&tokenIn=${tokenIn}&amountIn=${amountIn}&tokenOut=${tokenOut}&slippage=${slippage}&trade=${trade}`,
+}
+
+export default zapper
