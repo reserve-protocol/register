9ccb4ab562fbc97c113a74c1e4738f3412282253
Luis <luiska1993@gmail.com>
2024-12-06 15:42:21 -0600

    chore: token meta overview

--- src/views/dtf/overview/index.tsx
+++ src/views/dtf/overview/index.tsx
@@ -1,75 +1,154 @@
-import { Card } from '@/components/ui/card'
-import LandingMint from './components/landing-mint'
 import { Box } from '@/components/ui/box'
-import { Link as LinkIcon } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { Card, CardHeader } from '@/components/ui/card'
 import { Link } from '@/components/ui/link'
 import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from '@/components/ui/chart'
-import {
-  AreaChart,
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  XAxis,
-} from 'recharts'
+  ArrowUpRight,
+  Fingerprint,
+  Link as LinkIcon,
+  X,
+  XIcon,
+} from 'lucide-react'
+import LandingMint from './components/landing-mint'
 import PriceChart from './components/price-chart'
+import { useAtomValue } from 'jotai'
+import {
+  fTokenAtom,
+  fTokenConfigurationAtom,
+  fTokenGovernanceAtom,
+  fTokenMetaAtom,
+} from '@/state/dtf/atoms'
+import { Skeleton } from '@/components/ui/skeleton'
 
-const chartData = [
-  { month: 'January', desktop: 186 },
-  { month: 'February', desktop: 305 },
-  { month: 'March', desktop: 237 },
-  { month: 'April', desktop: 73 },
-  { month: 'May', desktop: 209 },
-  { month: 'June', desktop: 214 },
-]
+const DEFAULT_LOGO = 'https://storage.reserve.org/dtf-default.png'
 
-const data = [
-  { timestamp: 1, value: 280 },
-  { timestamp: 2, value: 275 },
-  { timestamp: 3, value: 285 },
-  { timestamp: 4, value: 290 },
-  { timestamp: 5, value: 288 },
-  { timestamp: 6, value: 100 },
-  { timestamp: 7, value: 295 },
-  { timestamp: 8, value: 500 },
-  { timestamp: 9, value: 287 },
-  { timestamp: 10, value: 290 },
-  { timestamp: 11, value: 295 },
-  { timestamp: 12, value: 304.03 },
-]
+const TokenSocials = () => {
+  const data = useAtomValue(fTokenMetaAtom)
 
-const timeRanges = [
-  { label: '1D', value: '1d' },
-  { label: '1W', value: '1w' },
-  { label: '1M', value: '1m' },
-  { label: '1Y', value: '1y' },
-  { label: 'ALL', value: 'all' },
-]
+  if (!data) {
+    return <Skeleton className="w-60 h-6" />
+  }
 
-const chartConfig = {
-  desktop: {
-    label: 'Desktop',
-    color: 'hsl(var(--card))',
-  },
-} satisfies ChartConfig
-
-const TokenDetails = () => {
   return (
-    <Card className="p-6 h-96">
-      <div className="flex">
-        card title
+    <div className="flex gap-3">
+      {data.website && (
         <Link>
           <Box variant="circle">
             <LinkIcon size={12} />
           </Box>
           Website
         </Link>
+      )}
+      {data.telegram && (
+        <Link>
+          <Box variant="circle">
+            <LinkIcon size={12} />
+          </Box>
+          Telegram
+        </Link>
+      )}
+      {data.twitter && (
+        <Link>
+          <Box variant="circle">
+            <X size={12} />
+          </Box>
+          Twitter
+        </Link>
+      )}
+    </div>
+  )
+}
+
+const TokenNameSkeleton = () => (
+  <div className="flex flex-col gap-4">
+    <Skeleton className="w-24 h-5" />
+    <Skeleton className="h-12 w-52 max-w-full" />
+  </div>
+)
+
+const TokenDetails = () => {
+  const data = useAtomValue(fTokenAtom)
+  const meta = useAtomValue(fTokenMetaAtom)
+  const governance = useAtomValue(fTokenGovernanceAtom)
+
+  return (
+    <Card className="p-6">
+      <div className="flex items-center mb-24">
+        <div className="mr-auto">
+          <img
+            src={meta?.logo || DEFAULT_LOGO}
+            alt={data?.symbol ?? 'dtf token logo'}
+            className="h-8 w-8 rounded-full"
+          />
+        </div>
+        <TokenSocials />
+      </div>
+      {!data ? (
+        <TokenNameSkeleton />
+      ) : (
+        <>
+          <h4>${data.symbol}</h4>
+          <h1 className="mt-4 text-4xl font-medium">{data.name}</h1>
+        </>
+      )}
+      <div className="flex items-center gap-1 mt-4">
+        <span className="text-legend">Owner</span>
+        {!governance ? (
+          <Skeleton className="w-30 h-5" />
+        ) : (
+          <Link>
+            <span className="font-bold">{governance.deployer}</span>
+            <Box variant="circle">
+              <ArrowUpRight size={12} />
+            </Box>
+          </Link>
+        )}
+      </div>
+    </Card>
+  )
+}
+
+const TokenAbout = () => {
+  const data = useAtomValue(fTokenMetaAtom)
+  const config = useAtomValue(fTokenConfigurationAtom)
+
+  return (
+    <Card className="p-6">
+      <div className="flex items-center gap-2  mb-24">
+        <div className="rounded-full border border-foreground p-2 mr-auto">
+          <Fingerprint size={20} />
+        </div>
+
+        {!config ? (
+          <Skeleton className="w-60 h-6" />
+        ) : (
+          <div className="flex gap-4">
+            <div className="flex gap-1">
+              <Box variant="circle">
+                <ArrowUpRight size={12} />
+              </Box>
+              <span className="font-medium">
+                {config.IsManaged ? 'Managed' : 'Volatile'}
+              </span>
+            </div>
+            <div className="flex gap-1">
+              <Box variant="circle">
+                <ArrowUpRight size={12} />
+              </Box>
+              <span className="text-legend">TVL Fee:</span>
+              <span className="font-bold">{config.fee}%</span>
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <h2 className="text-4xl font-medium mb-2">About this token</h2>
+        {!data ? (
+          <div>
+            <Skeleton className="w-full h-20" />
+          </div>
+        ) : (
+          <p className="text-legend">{data.description}</p>
+        )}
       </div>
     </Card>
   )
@@ -81,11 +160,7 @@ const Content = () => {
       <PriceChart />
       <div className="flex flex-col gap-1 m-1 -mt-20">
         <TokenDetails />
-        <TokenDetails />
-
-        <TokenDetails />
-        <TokenDetails />
-        <TokenDetails />
+        <TokenAbout />
       </div>
     </div>
   )
