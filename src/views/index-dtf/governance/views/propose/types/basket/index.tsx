b46192de9ee0146fb9aed3c2cd9dd380676f82a5
Luis <luiska1993@gmail.com>
2025-01-19 20:19:39 -0600

    chore: consider updater

--- src/views/index-dtf/governance/views/propose/types/basket/index.tsx
+++ src/views/index-dtf/governance/views/propose/types/basket/index.tsx
@@ -1,5 +1,36 @@
 import BasketProposalSteps from './components/basket-proposal-steps'
 import BasketProposalOverview from './components/basket-proposal-overview'
+import { useEffect } from 'react'
+import { useSetAtom } from 'jotai'
+import {
+  permissionlessLaunchingAtom,
+  proposedIndexBasketAtom,
+  stepAtom,
+  tradeRangeOptionAtom,
+  tradeVolatilityAtom,
+} from './atoms'
+
+// TODO: Get initial proposed basket HERE
+const Updater = () => {
+  const setStep = useSetAtom(stepAtom)
+  const setProposedBasket = useSetAtom(proposedIndexBasketAtom)
+  const setTradeVolatility = useSetAtom(tradeVolatilityAtom)
+  const setTradeRangeOption = useSetAtom(tradeRangeOptionAtom)
+  const setPermissionlessLaunching = useSetAtom(permissionlessLaunchingAtom)
+
+  useEffect(() => {
+    return () => {
+      setStep('basket')
+      // TODO: Currently using mock data!
+      // setProposedBasket(undefined)
+      setTradeVolatility([])
+      setTradeRangeOption(undefined)
+      setPermissionlessLaunching(undefined)
+    }
+  }, [])
+
+  return null
+}
 
 const IndexDTFBasketProposal = () => (
   <div className="grid grid-cols-[1.5fr_1fr] gap-2 pr-2 pb-6">
@@ -7,6 +38,7 @@ const IndexDTFBasketProposal = () => (
     <div>
       <BasketProposalOverview />
     </div>
+    <Updater />
   </div>
 )
 
