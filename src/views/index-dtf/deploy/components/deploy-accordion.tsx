98e9635a643e4fa00f07a0757aa130d228675650
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-01 14:27:50 -0300

    chore: update copy

--- src/views/index-dtf/deploy/components/deploy-accordion.tsx
+++ src/views/index-dtf/deploy/components/deploy-accordion.tsx
@@ -65,7 +65,7 @@ export const DEPLOY_STEPS: DeployStep[] = [
   {
     id: 'governance',
     icon: <Asterisk size={24} strokeWidth={1.5} />,
-    title: 'Governance',
+    title: 'Governance Body',
     titleSecondary: 'Governance Type',
     content: <Governance />,
   },
@@ -93,15 +93,15 @@ export const DEPLOY_STEPS: DeployStep[] = [
   {
     id: 'basket-changes',
     icon: <Asterisk size={24} strokeWidth={1.5} />,
-    title: 'Basket Changes',
-    titleSecondary: 'Basket Changes',
+    title: 'Basket Governance Settings',
+    titleSecondary: 'Basket Governance Settings',
     content: <BasketChanges />,
   },
   {
     id: 'other-changes',
     icon: <Asterisk size={24} strokeWidth={1.5} />,
-    title: 'Other Governance Changes',
-    titleSecondary: 'Other Governance Changes',
+    title: 'Non-Basket Governance Settings',
+    titleSecondary: 'Non-Basket Governance Settings',
     content: <OtherChanges />,
   },
 ]
--- src/views/index-dtf/deploy/steps/basket-changes/index.tsx
+++ src/views/index-dtf/deploy/steps/basket-changes/index.tsx
@@ -3,8 +3,10 @@ import BasketChangesForm from './basket-changes-form'
 
 const Description = () => (
   <div className="px-6 pb-6 text-base">
-    Updates to the basket of an Index DTF go through their own governance
-    process. Below are a list of field specific to these types of proposals.
+    Updates to the basket of an Index DTF can be configured to happen at a
+    different speed from all other governance, in case you want to move quickly
+    for basket changes, or take extra time for basket changes. You’ll set these
+    same parameters for all other governance in the next section.
   </div>
 )
 
