e783f6f2e15a8c5a721def133a551ce9beb2c3b4
Jorge Galat <jrgglt@gmail.com>
2025-02-04 11:30:18 -0500

    feat: add tooltip to price chart

--- src/views/index-dtf/overview/components/price-chart.tsx
+++ src/views/index-dtf/overview/components/price-chart.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import { ChartConfig, ChartContainer } from '@/components/ui/chart'
 import { indexDTFAtom } from '@/state/dtf/atoms'
 import { useAtomValue } from 'jotai'
-import { Line, LineChart, YAxis } from 'recharts'
+import { Line, LineChart, Tooltip, YAxis } from 'recharts'
 import useIndexDTFPriceHistory, {
   IndexDTFPerformance,
 } from '../hooks/use-dtf-price-history'
@@ -10,7 +10,10 @@ import { useState } from 'react'
 import useIndexDTFCurrentPrice from '../hooks/use-dtf-price'
 import { Skeleton } from '@/components/ui/skeleton'
 import { formatCurrency } from '@/utils'
+import InfoBox from '@/components/old/info-box'
 import PriceTag from '@/components/icons/PriceTag'
+import { Card } from 'theme-ui'
+import dayjs from 'dayjs'
 
 const chartConfig = {
   desktop: {
@@ -19,7 +22,7 @@ const chartConfig = {
   },
 } satisfies ChartConfig
 
-type Range = '1d' | '1w' | '1m' | '1y' | 'all'
+type Range = '1d' | '1w' | '1m' | '1y'
 
 const now = Math.floor(Date.now() / 1_000)
 const currentHour = Math.floor(now / 3_600) * 3_600
@@ -27,9 +30,15 @@ const currentHour = Math.floor(now / 3_600) * 3_600
 const historicalConfigs = {
   '1d': { to: currentHour, from: currentHour - 86_400, interval: '1h' },
   '1w': { to: currentHour, from: currentHour - 604_800, interval: '1h' },
-  '1m': { to: currentHour, from: currentHour - 2_592_000, interval: '1d' },
+  '1m': { to: currentHour, from: currentHour - 2_592_000, interval: '1h' },
   '1y': { to: currentHour, from: currentHour - 31_536_000, interval: '1d' },
-  all: { to: currentHour, from: currentHour - 3 * 31_536_000, interval: '1d' },
+} as const
+
+const plabel = {
+  '1d': '24h',
+  '1w': '7d',
+  '1m': '30d',
+  '1y': '365d',
 } as const
 
 const timeRanges = [
@@ -37,7 +46,6 @@ const timeRanges = [
   { label: '1W', value: '1w' },
   { label: '1M', value: '1m' },
   { label: '1Y', value: '1y' },
-  { label: 'ALL', value: 'all' },
 ] as const
 
 const calculatePercentageChange = (
@@ -50,6 +58,24 @@ const calculatePercentageChange = (
   const lastValue = performance[performance.length - 1].price
   const percentageChange = ((lastValue - firstValue) / firstValue) * 100
   return `${percentageChange > 0 ? '+' : ''}${percentageChange.toFixed(2)}%`
+}
+
+function CustomTooltip({ payload, active }: any) {
+  if (active && payload) {
+    const subtitle = dayjs
+      .unix(+payload[0]?.payload?.timestamp)
+      .format('YYYY-M-D HH:mm:ss')
+    return (
+      <Card backgroundColor="white">
+        <InfoBox
+          title={'$' + formatCurrency(payload[0]?.payload?.price, 5)}
+          subtitle={subtitle}
+        />
+      </Card>
+    )
+  }
+
+  return null
 }
 
 const PriceChart = () => {
@@ -67,9 +93,7 @@ const PriceChart = () => {
     <div className="rounded-2xl rounded-b-none bg-[#021122] w-full p-6 pb-20 color-[#fff] h-[500px]">
       <div className="flex justify-between">
         <div>
-          <span className="text-white">
-            {range === 'all' ? 'All time' : range} performance
-          </span>
+          <span className="text-white">{plabel[range]} performance</span>
           <div className="mt-1 mb-3 text-5xl font-bold text-white">
             {history === undefined ? (
               <Skeleton className="min-w-[180px] h-[48px]" />
@@ -100,24 +124,27 @@ const PriceChart = () => {
           ))}
         </div>
       </div>
-      <ChartContainer config={chartConfig} className="h-96 w-full pb-28">
-        <LineChart data={history?.timeseries}>
-          <YAxis
-            dataKey="price"
-            hide
-            visibility="0"
-            domain={['dataMin', 'dataMax']}
-          />
-          <Line
-            type="monotone"
-            dataKey="price"
-            stroke="#ffffff"
-            strokeWidth={2}
-            dot={false}
-            isAnimationActive={false}
-          />
-        </LineChart>
-      </ChartContainer>
+      {history !== undefined && (
+        <ChartContainer config={chartConfig} className="h-96 w-full pb-28">
+          <LineChart data={history?.timeseries}>
+            <YAxis
+              dataKey="price"
+              hide
+              visibility="0"
+              domain={['dataMin', 'dataMax']}
+            />
+            <Tooltip content={<CustomTooltip />} />
+            <Line
+              type="monotone"
+              dataKey="price"
+              stroke="#ffffff"
+              strokeWidth={2}
+              dot={false}
+              isAnimationActive={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      )}
     </div>
   )
 }
