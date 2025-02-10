26ac21fc0f5a52ba06f1a982726c07853948269e
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-10 12:08:34 -0300

    chore: disable refresh button on invalid input

--- src/views/index-dtf/issuance/index.tsx
+++ src/views/index-dtf/issuance/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import useERC20Balance from '@/hooks/useERC20Balance'
 import { indexDTFAtom } from '@/state/dtf/atoms'
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import {
   currentZapMintTabAtom,
   indexDTFBalanceAtom,
@@ -14,9 +16,6 @@ import {
 import Buy from '../overview/components/zap-mint/buy'
 import RefreshQuote from '../overview/components/zap-mint/refresh-quote'
 import Sell from '../overview/components/zap-mint/sell'
-import { Button } from '@/components/ui/button'
-import { ROUTES } from '@/utils/constants'
-import { Link } from 'react-router-dom'
 
 const IndexDTFIssuance = () => {
   const [currentTab, setCurrentTab] = useAtom(currentZapMintTabAtom)
@@ -24,8 +23,9 @@ const IndexDTFIssuance = () => {
   const zapRefetch = useAtomValue(zapRefetchAtom)
   const zapFetching = useAtomValue(zapFetchingAtom)
   const zapOngoingTx = useAtomValue(zapOngoingTxAtom)
-  const setInput = useSetAtom(zapMintInputAtom)
+  const [input, setInput] = useAtom(zapMintInputAtom)
   const setIndexDTFBalance = useSetAtom(indexDTFBalanceAtom)
+  const invalidInput = isNaN(Number(input)) || Number(input) === 0
 
   const { data: balance } = useERC20Balance(indexDTF?.id)
 
@@ -36,7 +36,7 @@ const IndexDTFIssuance = () => {
   if (!indexDTF) return null
 
   return (
-    <div className="flex flex-col gap-4  mx-auto">
+    <div className="flex flex-col gap-4 sm:w-[560px] mx-auto">
       <div className="mt-14 bg-card rounded-xl p-2 border border-border mx-auto">
         <Tabs
           value={currentTab}
@@ -53,7 +53,7 @@ const IndexDTFIssuance = () => {
             </TabsList>
             <RefreshQuote
               onClick={zapRefetch.fn}
-              disabled={zapFetching || zapOngoingTx}
+              disabled={zapFetching || zapOngoingTx || invalidInput}
             />
           </div>
           <TabsContent
@@ -74,15 +74,17 @@ const IndexDTFIssuance = () => {
           </TabsContent>
         </Tabs>
       </div>
-      <div className="w-full sm:w-[560px] mx-auto rounded-3xl border p-4 flex items-center gap-2">
-        <p className="text-sm text-muted-foreground mr-auto">
-          Having issues {currentTab === 'buy' ? 'minting' : 'redeeming'}?
-        </p>
-        <Link to={`./manual`}>
-          <Button variant="muted" size="xs">
-            Switch to manual {currentTab === 'buy' ? 'minting' : 'redeeming'}
-          </Button>
-        </Link>
+      <div className="w-full sm:w-[560px] mx-auto">
+        <div className="rounded-xl border p-4 flex items-center gap-2 mx-0.5">
+          <p className="text-sm text-muted-foreground mr-auto">
+            Having issues {currentTab === 'buy' ? 'minting' : 'redeeming'}?
+          </p>
+          <Link to={`./manual`}>
+            <Button variant="muted" size="xs">
+              Switch to manual {currentTab === 'buy' ? 'minting' : 'redeeming'}
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   )
