2ac0d2c88a4d2bd0377aaf8e43b4bcd2d9cf4ba6
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-13 14:21:40 -0300

    chore: change collect dust help text

--- src/views/index-dtf/overview/components/zap-mint/zap-settings.tsx
+++ src/views/index-dtf/overview/components/zap-mint/zap-settings.tsx
@@ -26,7 +26,7 @@ const ZapSettings = () => {
       <div className="flex flex-col gap-2">
         <ZapSettingsRowTitle
           title="Collect dust?"
-          help="The maximum amount of slippage you are willing to accept when minting. Higher slippage settings will make the transaction more likely to succeed, but may result in fewer tokens minted."
+          help="Dust is the leftover amount of tokens that cannot be exchanged. If you choose to collect dust, it will be sent back to your wallet. Sending dust back to the wallet will increase transaction fee."
         />
         <div className="rounded-xl border border-border pl-2 pr-3 py-3 flex items-center gap-1 justify-between">
           <div className="flex items-center gap-1">
