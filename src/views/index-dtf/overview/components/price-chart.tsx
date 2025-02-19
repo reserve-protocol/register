5cad5d6d483615d0c9816728b2a2d5675c19ce60
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-19 15:20:54 -0300

    chore: hide 1y time range for price chart

--- src/views/index-dtf/overview/components/price-chart.tsx
+++ src/views/index-dtf/overview/components/price-chart.tsx
@@ -44,7 +44,7 @@ const timeRanges = [
   { label: '1D', value: '1d' },
   { label: '1W', value: '1w' },
   { label: '1M', value: '1m' },
-  { label: '1Y', value: '1y' },
+  // { label: '1Y', value: '1y' },
 ] as const
 
 const calculatePercentageChange = (
