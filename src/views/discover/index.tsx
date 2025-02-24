f24fabb6060aaaaf2ae05990f7b831acae83a874
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-24 08:50:06 -0700

    chore: add feedback on discover page

--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -2,10 +2,12 @@ import HistoricalTVL from './components/historical-tvl'
 import DiscoverTabs from './components/discover-tabs'
 import RegisterAbout from './components/yield/components/RegisterAbout'
 import Splash from './components/splash'
+import FeedbackButton from '@/components/feedback-button'
 
 const Discover = () => {
   return (
     <>
+      <FeedbackButton />
       <HistoricalTVL />
       <DiscoverTabs className="mt-6 sm:mt-12" />
       <div className="container">
