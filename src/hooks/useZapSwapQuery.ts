7e715882e574b0cc096ebce7e62dde2bbe8279ba
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-25 14:41:30 -0700

    chore: add bypass cache to zapper

--- src/hooks/useZapSwapQuery.ts
+++ src/hooks/useZapSwapQuery.ts
@@ -4,12 +4,11 @@ import zapper, {
   ZapResponse,
 } from '@/views/yield-dtf/issuance/components/zapV2/api'
 import { useQuery } from '@tanstack/react-query'
-import { useAtom, useAtomValue, useSetAtom } from 'jotai'
-import { useEffect, useMemo } from 'react'
-import { Address, formatEther } from 'viem'
-import useDebounce from './useDebounce'
+import { useAtomValue, useSetAtom } from 'jotai'
 import mixpanel from 'mixpanel-browser/src/loaders/loader-module-core'
-import { indexDTFAtom, indexDTFPriceAtom } from '@/state/dtf/atoms'
+import { useEffect, useMemo } from 'react'
+import { Address } from 'viem'
+import useDebounce from './useDebounce'
 
 const DUST_REFRESH_THRESHOLD = 0.025
 const PRICE_IMPACT_THRESHOLD = 2
@@ -33,30 +32,29 @@ const useZapSwapQuery = ({
   dtfTicker: string
   type: 'buy' | 'sell'
 }) => {
-  const indexDTFPrice = useAtomValue(indexDTFPriceAtom)
   const chainId = useAtomValue(chainIdAtom)
   const account = useAtomValue(walletAtom)
   const setZapSwapEndpoint = useSetAtom(zapSwapEndpointAtom)
 
+  const getEndpoint = (bypassCache = false) =>
+    !tokenIn || !tokenOut || isNaN(Number(amountIn)) || Number(amountIn) === 0
+      ? null
+      : zapper.zap({
+          chainId,
+          tokenIn,
+          tokenOut,
+          amountIn,
+          slippage,
+          signer: account as Address,
+          trade: !forceMint,
+          bypassCache,
+        })
+
   const endpoint = useDebounce(
-    useMemo(() => {
-      if (
-        !tokenIn ||
-        !tokenOut ||
-        isNaN(Number(amountIn)) ||
-        Number(amountIn) === 0
-      )
-        return null
-      return zapper.zap({
-        chainId,
-        tokenIn,
-        tokenOut,
-        amountIn,
-        slippage,
-        signer: account as Address,
-        trade: !forceMint,
-      })
-    }, [chainId, account, tokenIn, tokenOut, amountIn, slippage, forceMint]),
+    useMemo(
+      () => getEndpoint(false),
+      [chainId, account, tokenIn, tokenOut, amountIn, slippage, forceMint]
+    ),
     500
   )
 
@@ -76,7 +74,13 @@ const useZapSwapQuery = ({
       let lastData: ZapResponse
 
       while (true) {
-        const response = await fetch(endpoint!)
+        // Bypass cache if price impact > threshold
+        const currentEndpoint =
+          priceImpactAttempt > 0 ? getEndpoint(true) : endpoint
+
+        if (!currentEndpoint) throw new Error('No endpoint available')
+
+        const response = await fetch(currentEndpoint)
         if (!response.ok) {
           const error = response.status
           mixpanel.track('index-dtf-zap-swap', {
@@ -86,7 +90,7 @@ const useZapSwapQuery = ({
             ticker: dtfTicker,
             chainId,
             type,
-            endpoint: endpoint,
+            endpoint: currentEndpoint,
             status: 'error',
             tokenIn,
             tokenOut,
@@ -104,7 +108,7 @@ const useZapSwapQuery = ({
             ticker: dtfTicker,
             chainId,
             type,
-            endpoint: endpoint,
+            endpoint: currentEndpoint,
             status: data.status,
             tokenIn,
             tokenOut,
@@ -121,12 +125,7 @@ const useZapSwapQuery = ({
 
         lastData = data
 
-        if (
-          data &&
-          data.result &&
-          data.result.amountOutValue &&
-          data.result.dustValue
-        ) {
+        if (data && data.result) {
           const amountOut = Number(data.result.amountOutValue)
           const dust = Number(data.result.dustValue)
           const priceImpact = Number(data.result.truePriceImpact)
@@ -142,7 +141,7 @@ const useZapSwapQuery = ({
           const isPriceImpactRetry =
             priceImpact > PRICE_IMPACT_THRESHOLD &&
             priceImpactAttempt < maxPriceImpactRetries
-
+          console.log('isPriceImpactRetry', isPriceImpactRetry)
           if (
             priceImpactAttempt < maxPriceImpactRetries &&
             isPriceImpactRetry
@@ -152,70 +151,6 @@ const useZapSwapQuery = ({
           }
         }
         break
-      }
-
-      if (
-        indexDTFPrice &&
-        lastData?.result &&
-        [
-          '0xf91384484f4717314798e8975bcd904a35fc2bf1',
-          '0x4e3b170dcbe704b248df5f56d488114ace01b1c5',
-        ].includes(tokenOut?.toLowerCase() || '')
-      ) {
-        const amountOutValue =
-          indexDTFPrice * Number(formatEther(BigInt(lastData.result.amountOut)))
-        const amountInValue = Number(lastData.result.amountInValue)
-        const dustValue = Number(lastData.result.dustValue) // TODO: calculate from dust
-
-        const diff = Math.abs(amountOutValue + dustValue - amountInValue)
-        const truePriceImpact =
-          amountOutValue + dustValue > amountInValue
-            ? -(diff / amountInValue) * 100
-            : (diff / amountInValue) * 100
-
-        const priceImpact = truePriceImpact < 0 ? 0 : truePriceImpact
-
-        lastData = {
-          ...lastData,
-          result: {
-            ...lastData.result,
-            amountOutValue,
-            truePriceImpact,
-            priceImpact,
-          },
-        }
-      }
-
-      if (
-        indexDTFPrice &&
-        lastData?.result &&
-        [
-          '0xf91384484f4717314798e8975bcd904a35fc2bf1',
-          '0x4e3b170dcbe704b248df5f56d488114ace01b1c5',
-        ].includes(tokenIn?.toLowerCase() || '')
-      ) {
-        const amountOutValue = Number(lastData.result.amountOutValue)
-        const amountInValue =
-          indexDTFPrice * Number(formatEther(BigInt(lastData.result.amountIn)))
-        const dustValue = Number(lastData.result.dustValue) // TODO: calculate from dust
-
-        const diff = Math.abs(amountOutValue + dustValue - amountInValue)
-        const truePriceImpact =
-          amountOutValue + dustValue > amountInValue
-            ? -(diff / amountInValue) * 100
-            : (diff / amountInValue) * 100
-
-        const priceImpact = truePriceImpact < 0 ? 0 : truePriceImpact
-
-        lastData = {
-          ...lastData,
-          result: {
-            ...lastData.result,
-            amountOutValue,
-            truePriceImpact,
-            priceImpact,
-          },
-        }
       }
 
       return lastData
