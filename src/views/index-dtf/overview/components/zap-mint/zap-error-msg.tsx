f0058facb27289cd15103114c17318860e760011
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-02-25 10:00:26 -0700

    chore: zap redeem callout

--- src/views/index-dtf/overview/components/zap-mint/zap-error-msg.tsx
+++ src/views/index-dtf/overview/components/zap-mint/zap-error-msg.tsx
@@ -1,7 +1,12 @@
 import TransactionError from '@/components/transaction-error/TransactionError'
 import { useAtomValue } from 'jotai'
-import { zapSwapEndpointAtom } from './atom'
+import { currentZapMintTabAtom, zapSwapEndpointAtom } from './atom'
 import Copy from '@/components/ui/copy'
+import { Link } from 'react-router-dom'
+import { getFolioRoute } from '@/utils'
+import { indexDTFAtom } from '@/state/dtf/atoms'
+import { ROUTES } from '@/utils/constants'
+import { Button } from '@/components/ui/button'
 
 const SWAP_ERROR_MSG =
   'Sorry, we’re having a hard time finding a route that makes sense for you. Please try again in a bit.'
@@ -24,18 +29,46 @@ const CopySwapButton = () => {
   )
 }
 
+const GoToManualRedeem = () => {
+  const indexDTF = useAtomValue(indexDTFAtom)
+  const currentTab = useAtomValue(currentZapMintTabAtom)
+  const isRedeem = currentTab === 'sell'
+
+  if (!isRedeem || !indexDTF) return null
+
+  return (
+    <div className="mt-2 hidden sm:block">
+      <Button size="sm" className="w-full" asChild>
+        <Link
+          to={getFolioRoute(
+            indexDTF.id,
+            indexDTF.chainId,
+            `${ROUTES.ISSUANCE}/manual`
+          )}
+        >
+          Try redeeming manually
+        </Link>
+      </Button>
+    </div>
+  )
+}
+
 const ZapErrorMsg = ({ error }: { error?: string }) => {
   if (!error) return null
+
   const errorMsg =
     Object.entries(ERROR_MAP).find(([key]) =>
       error.toLowerCase().includes(key.toLowerCase())
     )?.[1] || error
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
-      <div className="text-red-500 text-sm text-center mt-2">{errorMsg}</div>
-      <CopySwapButton />
-    </div>
+    <>
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <div className="text-red-500 text-sm text-center mt-2">{errorMsg}</div>
+        <CopySwapButton />
+      </div>
+      <GoToManualRedeem />
+    </>
   )
 }
 
