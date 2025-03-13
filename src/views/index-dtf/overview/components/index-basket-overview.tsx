564161f7a0eee938e99667e1aa1d8a6cd177f52d
Luis <luiska1993@gmail.com>
2025-03-13 12:03:13 -0600

    chore: fix overflows

--- src/views/index-dtf/overview/components/index-basket-overview.tsx
+++ src/views/index-dtf/overview/components/index-basket-overview.tsx
@@ -39,8 +39,8 @@ const BasketSkeleton = () =>
       <TableCell className="flex items-center gap-3">
         <Skeleton className="h-8 w-8 rounded-full" />
         <div className="flex flex-col gap-1">
-          <Skeleton className="h-4 w-[120px]" />
-          <Skeleton className="h-3 w-[80px]" />
+          <Skeleton className="h-4 w-12 lg:w-[120px]" />
+          <Skeleton className="h-3 w-8 lg:w-[80px]" />
         </div>
       </TableCell>
       <TableCell className="hidden sm:table-cell">
