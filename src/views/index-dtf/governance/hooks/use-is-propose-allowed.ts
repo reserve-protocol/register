e9133190f5c84ae61fd0d2583c9d396c70a0c2e1
Jorge Galat <jrgglt@gmail.com>
2025-04-14 10:22:51 -0400

    fix: voting power blink (#675)

--- src/views/index-dtf/governance/hooks/use-is-propose-allowed.ts
+++ src/views/index-dtf/governance/hooks/use-is-propose-allowed.ts
@@ -3,17 +3,22 @@ import { useAtomValue } from 'jotai'
 import { useVotingPower } from './use-voting-power'
 import { indexGovernanceOverviewAtom } from '../atoms'
 
-export const useIsProposeAllowed = (): boolean | undefined => {
+export const useIsProposeAllowed = () => {
   const dtf = useAtomValue(indexDTFAtom)
   const governance = useAtomValue(indexGovernanceOverviewAtom)
-  const votingPower = useVotingPower()
+  const { votingPower, isLoading } = useVotingPower()
 
   const voteSupply = governance?.voteSupply
   const proposalThreshold = dtf?.ownerGovernance?.proposalThreshold
 
-  return (
-    !!voteSupply &&
-    !!proposalThreshold &&
-    votingPower / voteSupply >= proposalThreshold / 1e20
-  )
+  return {
+    isProposeAllowed:
+      !!voteSupply &&
+      !!proposalThreshold &&
+      votingPower / voteSupply >= proposalThreshold / 1e20,
+    isLoading:
+      isLoading ||
+      typeof voteSupply === 'undefined' ||
+      typeof proposalThreshold === 'undefined',
+  }
 }
