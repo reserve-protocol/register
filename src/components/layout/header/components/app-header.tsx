2fa0ace139fc50227c37955edc60cdbd299fd519
Jorge Galat <jrgglt@gmail.com>
2025-04-18 09:38:03 -0400

    feat: campaign (#678)

--- src/components/layout/header/components/app-header.tsx
+++ src/components/layout/header/components/app-header.tsx
@@ -1,12 +1,13 @@
 import Account from 'components/account'
 import Brand from './Brand'
-import CoinbaseSubscribe from './CoinbaseSubscribe'
 import { cn } from '@/lib/utils'
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
 import { useLocation } from 'react-router-dom'
 import AppNavigation from './app-navigation'
-import { useColorMode } from 'theme-ui'
 import ThemeColorMode from '@/components/dark-mode-toggle/ThemeColorMode'
+import { Button } from '@/components/ui/button'
+import { ArrowUpRight, Gem } from 'lucide-react'
+import { CAMPAIGN_URL } from '@/views/index-dtf/overview/hooks/use-campaign'
 
 const Container = ({ children }: { children: ReactNode }) => {
   // Check if the route is a "index-dtf" route
@@ -29,23 +30,34 @@ const AppHeader = () => (
     <div className="container flex items-center h-[56px] md:h-[72px] px-4 sm:px-6">
       <Brand className="text-primary mr-2 sm:mr-4 cursor-pointer md:-mt-1" />
       <AppNavigation />
-      <ThemeColorMode
-        sx={{
-          display: 'flex',
-          px: 2,
-          mr: [2, 3],
-          py: '3px',
-          maxWidth: '32px',
-          borderRadius: '6px',
-          ml: 'auto',
-          cursor: 'pointer',
-          ':hover': {
-            backgroundColor: 'secondaryBackground',
-          },
-        }}
-      />
-
-      <Account />
+      <div className="flex items-center">
+        <Button
+          className="hidden lg:flex items-center pl-3 py-2 pr-[10px] rounded-[12px] h-8 mr-2 bg-[#FFBE45] hover:bg-[#FFBE45]/90 text-black text-base font-light"
+          asChild
+        >
+          <a href={CAMPAIGN_URL} target="_blank">
+            <Gem size={16} />
+            <span className="ml-[6px] mr-1">$25K in Rewards</span>
+            <ArrowUpRight size={16} />
+          </a>
+        </Button>
+        <ThemeColorMode
+          sx={{
+            display: 'flex',
+            px: 2,
+            mr: [2, 3],
+            py: '3px',
+            maxWidth: '32px',
+            borderRadius: '6px',
+            ml: 'auto',
+            cursor: 'pointer',
+            ':hover': {
+              backgroundColor: 'secondaryBackground',
+            },
+          }}
+        />
+        <Account />
+      </div>
     </div>
   </Container>
 )
