9ea70965ef4f6eb844b3454fd4feaa060cfa5630
Luis <luiska1993@gmail.com>
2025-04-29 22:39:01 -0600

    chore: remove upgrade banners

--- src/views/index-dtf/governance/views/propose/components/proposal-type-selection.tsx
+++ src/views/index-dtf/governance/views/propose/components/proposal-type-selection.tsx
@@ -92,8 +92,8 @@ const ProposalTypeSelection = () => {
   return (
     <div className="flex h-[calc(100vh-146px)] lg:h-[calc(100vh-72px)] w-full">
       <div className="flex flex-col gap-4 items-center justify-center m-4 flex-grow border-none sm:border-dashed border-2 border-foreground/40 rounded-3xl">
-        <ProposeIndexUpgrade />
-        <ProposeGovernanceSpell31032025 />
+        {/* <ProposeIndexUpgrade />
+        <ProposeGovernanceSpell31032025 /> */}
         <div className="bg-secondary rounded-3xl w-full sm:w-[408px]">
           <Header />
           <TypeList />
