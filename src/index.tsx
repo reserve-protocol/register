f0599ebe17f360168711387e126da5dcce048176
Luis <luiska1993@gmail.com>
2025-05-07 10:48:49 -0600

    chore: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import 'polyfills'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://107782f294b8cc0089b1b8ed58f2b917@o4509282817015808.ingest.us.sentry.io/4509282817736704',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+})
 
 const root = createRoot(document.getElementById('root')!)
 
