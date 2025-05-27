080aece9fb8beeddf6c937d6edb09056e1882b70
Luis <luiska1993@gmail.com>
2025-05-27 14:27:29 -0600

    chore: add deprecated auctions tweaks

--- src/views/index-dtf/governance/views/propose/basket/legacy-atoms.ts
+++ src/views/index-dtf/governance/views/propose/basket/legacy-atoms.ts
@@ -62,7 +62,7 @@ export const ttlATom = atom((get) => {
   return isNaN(window) ? tradeDelay : tradeDelay + BigInt(Math.round(window))
 })
 
-const VOLATILITY_VALUES = [0.1, 0.2, 0.5]
+const VOLATILITY_VALUES = [0.15, 0.4, 0.8]
 
 function getProposedTrades(get: Getter, deferred = false) {
   const proposedBasket = get(proposedIndexBasketAtom)
@@ -115,8 +115,11 @@ function getProposedTrades(get: Getter, deferred = false) {
 
     prices.push(priceMap[asset] ?? 0)
 
-    // TODO: assume trades always have the same order...
-    error.push(deferred ? 1 : VOLATILITY_VALUES[volatility[index] || 0] || 0.1)
+    error.push(
+      deferred
+        ? VOLATILITY_VALUES[2]
+        : VOLATILITY_VALUES[volatility[index] || 1] || 0.15
+    )
 
     index++
   }
@@ -129,7 +132,9 @@ function getProposedTrades(get: Getter, deferred = false) {
     targetBasket,
     prices,
     error,
-    dtfPrice
+    dtfPrice,
+    3n * 10n ** 14n,
+    deferred
   )
 }
 
