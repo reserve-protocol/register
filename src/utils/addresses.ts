c788283cf09afaf07707e3ce3455251644a5988d
Luis <luiska1993@gmail.com>
2025-06-05 14:15:43 -0600

    chore: update base addresses

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -34,13 +34,13 @@ export const FACADE_WRITE_ADDRESS: AddressMap = {
 
 export const INDEX_DEPLOYER_ADDRESS: AddressMap = {
   [ChainId.Mainnet]: '0xaafb13a3df7cE70c140E40c959D58Fd5Cc443Cba', // TODO: still 2.0
-  [ChainId.Base]: '0xa70e7f86c56bf63befee3ef76ca0d392acab8ef2', // 4,0
+  [ChainId.Base]: '0xBE3B47587cEeff7D48008A0114f51cD571beC63A', // 4,0
   [ChainId.Arbitrum]: '0x',
 }
 
 export const INDEX_GOVERNANCE_DEPLOYER_ADDRESS: AddressMap = {
   [ChainId.Mainnet]: '0xE926577a152fFD5f5036f88BF7E8E8D3652B558C',
-  [ChainId.Base]: '0x19E3d9Fbd121A13bB55BCb6B50F9cb44F9306ce0',
+  [ChainId.Base]: '0x5Bed18AcA50E6057E6658Fe8498004092EedCDcF', // 4.0
   [ChainId.Arbitrum]: '0x',
 }
 
