46b06530e573aa56d932eb24f8c9f731b50c2557
Luis <luiska1993@gmail.com>
2025-06-11 12:54:31 -0600

    chore: fix container

--- src/views/index-dtf/auctions/index.tsx
+++ src/views/index-dtf/auctions/index.tsx
@@ -7,7 +7,7 @@ const IndexDTFAuctions = () => {
 
   return (
     <div className="container">
-      <div className="flex flex-col items-center sm:justify-start md:justify-center gap-2 lg:bg-secondary/70 border-2 border-secondary lg:h-[calc(100vh-80px)] dark:bg-card rounded-4xl sm:mr-2 overflow-auto">
+      <div className="flex flex-col items-center sm:justify-start md:justify-center gap-2 lg:bg-secondary/70 lg:border-2 border-secondary lg:min-h-[calc(100vh-80px)] dark:bg-card rounded-4xl sm:mr-2 py-4">
         <Outlet />
         <Updater />
       </div>
@@ -16,3 +16,10 @@ const IndexDTFAuctions = () => {
 }
 
 export default IndexDTFAuctions
+
+{
+  /* <div className="flex items-center justify-center lg:min-h-[calc(100vh-72px)] bg-secondary">
+<div className="w-10 h-[1000px]">other thing</div>
+container
+</div> */
+}
