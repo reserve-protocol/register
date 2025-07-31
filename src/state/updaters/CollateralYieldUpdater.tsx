8550daf0cb31cad4e2cd641896a031b6c40aaf39
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-07-30 22:28:46 -0300

    chore: update wsamm-eusd/usdc defillama id

--- src/state/updaters/CollateralYieldUpdater.tsx
+++ src/state/updaters/CollateralYieldUpdater.tsx
@@ -67,7 +67,7 @@ const poolsMap: Record<number, Record<string, string>> = {
     '9d09b0be-f6c2-463a-ad2c-4552b3e12bd9': 'wsgusdbc',
     '7e0661bf-8cf3-45e6-9424-31916d4c7b84': 'sabasusdc',
     '833ec61b-f9e6-46ac-9eff-2785808b2389': 'sabasusdbc',
-    '212375c8-694c-4fa5-8b36-05f50c8e61b2': 'wsamm-eusd/usdc',
+    'aeb97af6-bd75-4a34-975b-9166427a0ce3': 'wsamm-eusd/usdc',
     'b90eba2e-ed29-414e-b16d-82f9c3eae707': 'meusd',
     '1b52ab3b-98d5-4951-92d8-97dfca06e62f': 'wvamm-weth/aero',
     '029c10b3-8399-4a89-818a-935a4cb3e1da': 'wvamm-mog/weth',
