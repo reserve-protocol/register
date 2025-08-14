6af05543946d75c2a6031dcd655f10596624cee5
Luis <luiska1993@gmail.com>
2025-08-14 11:33:22 -0700

    chore: remove console

--- src/views/index-dtf/governance/views/propose/basket/components/submit-proposal-button.tsx
+++ src/views/index-dtf/governance/views/propose/basket/components/submit-proposal-button.tsx
@@ -32,8 +32,6 @@ const tradingGovAddress = atom((get) => {
 const ProposeGatekeeper = memo(() => {
   const { isProposeAllowed, isLoading } = useIsProposeAllowed()
 
-  console.log('isProposeAllowed', isProposeAllowed)
-
   if (!isLoading && !isProposeAllowed) {
     return (
       <Button disabled className="w-full" variant="default">
