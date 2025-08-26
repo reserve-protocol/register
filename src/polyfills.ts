e7b25693f2e597bb66e7134b8107c9e66aaafeaa
Luis <luiska1993@gmail.com>
2025-08-26 12:04:12 -0700

    chore: improve issuance layout

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,11 +4,4 @@ window.global = window.global ?? window
 window.Buffer = window.Buffer ?? Buffer
 window.process = window.process ?? { env: {} } // Minimal process polyfill
 
-// TODO: Typing for coinbase wallet integration?
-declare global {
-  interface Window {
-    CBWSubscribe: any
-  }
-}
-
 export {}
