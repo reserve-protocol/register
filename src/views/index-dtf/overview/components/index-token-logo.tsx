7734ecea8b46251e1ad7aa5406b7bf89e1185066
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-09-25 12:40:09 -0300

    chore: chain logo

--- src/views/index-dtf/overview/components/index-token-logo.tsx
+++ src/views/index-dtf/overview/components/index-token-logo.tsx
@@ -1,3 +1,4 @@
+import ChainLogo from '@/components/icons/ChainLogo'
 import TokenLogo from '@/components/token-logo'
 import { Skeleton } from '@/components/ui/skeleton'
 import { indexDTFAtom, indexDTFBrandAtom } from '@/state/dtf/atoms'
@@ -11,11 +12,19 @@ const IndexTokenLogo = () => {
     return <Skeleton className="h-8 w-8 rounded-full" />
   }
   return (
-    <TokenLogo
-      src={brand?.dtf?.icon || undefined}
-      alt={dtf?.token.symbol ?? 'dtf token logo'}
-      size="xl"
-    />
+    <div className="relative">
+      <TokenLogo
+        src={brand?.dtf?.icon || undefined}
+        alt={dtf?.token.symbol ?? 'dtf token logo'}
+        size="xl"
+      />
+      {dtf?.chainId && (
+        <ChainLogo
+          chain={dtf?.chainId}
+          className="absolute -bottom-1 -right-1"
+        />
+      )}
+    </div>
   )
 }
 
