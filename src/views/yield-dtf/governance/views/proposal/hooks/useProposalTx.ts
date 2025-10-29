1a9d97023d9cd6ba9164ffb4591940c7203cf5aa
Juan Pablo Rombola <juampi.rombola@gmail.com>
2025-10-29 16:31:50 -0300

    chore: update distributor and setDistributions

--- src/views/yield-dtf/governance/views/proposal/hooks/useProposalTx.ts
+++ src/views/yield-dtf/governance/views/proposal/hooks/useProposalTx.ts
@@ -489,53 +489,35 @@ const useProposalTx = () => {
       ############################# */
       if (revenueChanges.count) {
         const [dist, beneficiaries] = getSharesFromSplit(revenueSplit)
+        const distributionAddresses: Address[] = []
+        const distributionShares: { rTokenDist: number; rsrDist: number }[] = []
 
         for (const revChange of revenueChanges.externals) {
           if (!revChange.isNew) {
-            addresses.push(contracts.distributor.address)
-            calls.push(
-              encodeFunctionData({
-                abi: Distributor,
-                functionName: 'setDistribution',
-                args: [
-                  revChange.split.address as Address,
-                  { rTokenDist: 0, rsrDist: 0 },
-                ],
-              })
-            )
+            distributionAddresses.push(revChange.split.address as Address)
+            distributionShares.push({ rTokenDist: 0, rsrDist: 0 })
           }
         }
 
-        addresses.push(contracts.distributor.address)
-        calls.push(
-          encodeFunctionData({
-            abi: Distributor,
-            functionName: 'setDistribution',
-            args: [
-              FURNACE_ADDRESS,
-              { rTokenDist: dist.rTokenDist, rsrDist: 0 },
-            ],
-          })
-        )
-        addresses.push(contracts.distributor.address)
-        calls.push(
-          encodeFunctionData({
-            abi: Distributor,
-            functionName: 'setDistribution',
-            args: [ST_RSR_ADDRESS, { rTokenDist: 0, rsrDist: dist.rsrDist }],
-          })
-        )
+        distributionAddresses.push(FURNACE_ADDRESS)
+        distributionShares.push({ rTokenDist: dist.rTokenDist, rsrDist: 0 })
+
+        distributionAddresses.push(ST_RSR_ADDRESS)
+        distributionShares.push({ rTokenDist: 0, rsrDist: dist.rsrDist })
 
         for (const external of beneficiaries) {
-          addresses.push(contracts.distributor.address)
-          calls.push(
-            encodeFunctionData({
-              abi: Distributor,
-              functionName: 'setDistribution',
-              args: [external.beneficiary, external.revShare],
-            })
-          )
+          distributionAddresses.push(external.beneficiary)
+          distributionShares.push(external.revShare)
         }
+
+        addresses.push(contracts.distributor.address)
+        calls.push(
+          encodeFunctionData({
+            abi: Distributor,
+            functionName: 'setDistributions',
+            args: [distributionAddresses, distributionShares],
+          })
+        )
       }
 
       /* ########################## 
