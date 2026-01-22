9eed0a19d868c1af207a60a5a69269fbfa50d6ee
Luis <luiska1993@gmail.com>
2026-01-21 18:00:43 -0800

    chore: refresh packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import 'polyfills'
-import * as Sentry from '@sentry/react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://b68198129f2305a28405a306efc7d779@o4509282817015808.ingest.us.sentry.io/4509282885566464',
+  sendDefaultPii: true,
+})
 
 const root = createRoot(document.getElementById('root')!)
 
 root.render(<App />)
-
-// Defer Sentry init to after first render to avoid blocking FCP
-requestIdleCallback(
-  () => {
-    Sentry.init({
-      dsn: 'https://b68198129f2305a28405a306efc7d779@o4509282817015808.ingest.us.sentry.io/4509282885566464',
-      sendDefaultPii: true,
-    })
-  },
-  { timeout: 2000 }
-)
