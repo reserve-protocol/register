8cca22e0bf96a6d28135d82f3cb26f31a3083baf
Luis <luiska1993@gmail.com>
2026-02-03 10:13:22 -0800

    feat: add rebalance error

--- src/views/index-dtf/auctions/views/rebalance/index.tsx
+++ src/views/index-dtf/auctions/views/rebalance/index.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from 'jotai'
-import { showManageWeightsViewAtom } from './atoms'
+import { rebalanceErrorAtom, showManageWeightsViewAtom } from './atoms'
 import ManageWeightsView from './components/manage-weights/manage-weights-view'
 import RebalanceAction from './components/rebalance-action'
 import RebalanceAuctions from './components/rebalance-auctions'
@@ -10,6 +10,19 @@ import RebalanceOverview from './components/rebalance-overview'
 import RebalanceProgress from './components/rebalance-progress'
 import useIsRebalanceCompleted from './hooks/use-is-rebalance-completed'
 import Updater from './updater'
+
+const RebalanceError = () => {
+  const rebalanceError = useAtomValue(rebalanceErrorAtom)
+
+  if (!rebalanceError) return null
+
+  return (
+    <div className='rounded-3xl p-3 border border-red-500 bg-red-500/10'>
+      <h1 className='font-semibold text-red-500'>Rebalance error</h1>
+      <p>{rebalanceError}</p>
+    </div>
+  )
+}
 
 const RebalanceContent = () => {
   const showManageWeights = useAtomValue(showManageWeightsViewAtom)
@@ -24,7 +37,8 @@ const RebalanceContent = () => {
   }
 
   return (
-    <div className="md:w-[480px] p-1 flex flex-col gap-1 bg-secondary rounded-3xl">
+    <div className="md:w-[480px] p-1 pt-4 flex flex-col gap-1 bg-secondary rounded-3xl">
+      <RebalanceError />
       <div className="bg-background rounded-3xl">
         <RebalanceHeader />
         <RebalanceProgress />
