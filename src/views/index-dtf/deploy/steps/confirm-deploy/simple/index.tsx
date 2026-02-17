1d283780c55d11fd16bf41aecf93b3d4295f89b7
Luis <luiska1993@gmail.com>
2026-02-16 18:22:17 -0800

    chore: fix deploy

--- src/views/index-dtf/deploy/steps/confirm-deploy/simple/index.tsx
+++ src/views/index-dtf/deploy/steps/confirm-deploy/simple/index.tsx
@@ -67,7 +67,7 @@ const SimpleIndexDeploy = () => {
   const zapDeployPayload = useAtomValue(zapDeployPayloadAtom)
   const tokens = useAtomValue(tokensAtom)
   const [ongoingTx, setOngoingTx] = useAtom(ongoingTxAtom)
-  const formChainId = form?.chain
+  const formChainId = form?.chain as number
 
   const url = form?.governanceWalletAddress
     ? zapper.zapDeployUngoverned(formChainId)
