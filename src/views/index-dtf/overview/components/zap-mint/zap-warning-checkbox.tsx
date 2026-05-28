3d6f9d238f00c60fbc6d3d0b6630ebc18eb148a6
Jorge Galat <jrgglt@gmail.com>
2026-05-28 17:57:56 -0300

    i18n: localize index-dtf overview zap-mint UI

--- src/views/index-dtf/overview/components/zap-mint/zap-warning-checkbox.tsx
+++ src/views/index-dtf/overview/components/zap-mint/zap-warning-checkbox.tsx
@@ -7,6 +7,7 @@ import {
   zapPriceImpactWarningCheckboxAtom,
 } from './atom'
 import { formatPercentage } from '@/utils'
+import { Trans } from '@lingui/react/macro'
 
 const PRICE_IMPACT_THRESHOLD = 5
 
@@ -30,11 +31,15 @@ const ZapPriceImpactWarningCheckbox = ({
       <div className="flex items-end gap-2 justify-between">
         <div className="max-w-sm">
           <div className="font-bold">
-            High price impact: {formatPercentage(Math.abs(priceImpact))}
+            <Trans>
+              High price impact: {formatPercentage(Math.abs(priceImpact))}
+            </Trans>
           </div>
           <div className="text-sm text-legend">
-            The price impact for this trade is very high. You will get
-            significantly less value than expected.
+            <Trans>
+              The price impact for this trade is very high. You will get
+              significantly less value than expected.
+            </Trans>
           </div>
         </div>
         <div className="flex items-center p-[6px] border border-border rounded-lg">
