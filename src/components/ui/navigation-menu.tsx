626686651cd41afdc32d52648ac744a018e7c4c4
wistec-bisrum <110302288+wistec-bisrum@users.noreply.github.com>
2026-06-16 19:23:27 +0000

    Add horizontal padding to nav menu

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -32,7 +32,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      'group flex flex-1 list-none items-center justify-center',
+      'group flex flex-1 list-none items-center justify-center px-2',
       className
     )}
     {...props}
