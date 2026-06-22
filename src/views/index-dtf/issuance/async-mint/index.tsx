6b86e87a8f9d676a77b63729e0d2a1f620a53a9f
Jorge Galat <jrgglt@gmail.com>
2026-06-22 16:49:38 -0300

    fix: allow scrolling the automated mint wizard on short viewports

--- src/views/index-dtf/issuance/async-mint/index.tsx
+++ src/views/index-dtf/issuance/async-mint/index.tsx
@@ -66,7 +66,7 @@ const AsyncMintWizard = () => {
         className={cn(
           'w-full mx-auto overflow-hidden rounded-3xl transition-[max-width] duration-500 ease-out',
           isWide
-            ? 'max-w-[1200px] lg:max-h-[calc(100vh-100px)]'
+            ? 'max-w-[1200px] lg:max-h-[calc(100vh-100px)] lg:overflow-y-auto'
             : 'max-w-[476px]'
         )}
       >
